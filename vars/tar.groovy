9fd843d7ac19448f940d66506d0baed480a18f5a
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2018-09-25 10:22:11 +0200

    quote paths

--- vars/tar.groovy
+++ vars/tar.groovy
@@ -12,8 +12,8 @@ def call(Map params = [:]) {
   def file = params.containsKey('file') ? params.file : 'archive.tgz'
   def archive = params.containsKey('archive') ? params.archive : true
   def dir = params.containsKey('dir') ? params.dir : "."
-  def pathPrefix = params.containsKey('pathPrefix') ? "cd " + params.pathPrefix + " && " : ""
-  sh pathPrefix + "tar -czf " + file + " " + dir
+  def pathPrefix = params.containsKey('pathPrefix') ? "cd '" + params.pathPrefix + "' && " : ""
+  sh pathPrefix + "tar -czf '" + file + "' '" + dir + "'"
   if(archive){
     archiveArtifacts(allowEmptyArchive: true, 
                           artifacts: file, 
