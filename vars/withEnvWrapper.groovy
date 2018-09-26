fe661781e8071df8c26d3c3ec132fd0de3f420d5
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2018-09-26 13:39:10 +0200

    wrapper step

---
+++ vars/withEnvWrapper.groovy
@@ -0,0 +1,27 @@
+def call(Closure body, boolean deleteDir=true, String unstashSrc=null) {
+  timestamps {
+    ansiColor('xterm') {
+      withEnv([
+        'VAULT_ROLE_ID_HEY_APM=35ad5918-eab7-c814-f8be-a305c811732e', 
+        'VAULT_SECRET_ID_HEY_APM=95d18733-44b5-53c3-89c5-91e27b29be4f', 
+        'VAULT_SECRET_ID=c50f028c-ba5d-a921-7869-c63966e3cd79', 
+        'VAULT_ADDR=https://secrets.elastic.co:8200', 
+        'JOB_GCS_CREDENTIALS=jenkins-gcs-plugin', 
+        'JOB_GCS_BUCKET=apm-ci-artifacts/jobs', 
+        'JOB_GIT_CREDENTIALS=f6c7695a-671e-4f4f-a331-acdce44ff9ba']) {
+          if(deleteDir){
+            deteleDir()
+          }
+          if(unstashSrc != null){
+            unstash unstashSrc
+          }
+          body()
+      }
+    }
+  }
+  /* TODO replace each variable with a secret text credential type, then use withCredentials step.
+  withCredentials([string(credentialsId: '6a80d11c-cb5f-4e40-8565-78e127610ef1', variable: 'VAULT_ROLE_ID_HEY_APM')]) {
+    // some block
+  }
+  */
+}
