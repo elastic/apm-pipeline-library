3203fc03e1f12e1692df326db79a06fb31005564
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2018-09-27 17:17:37 +0200

    test env

--- vars/withEnvWrapper.groovy
+++ vars/withEnvWrapper.groovy
@@ -7,6 +7,8 @@ withEnvWrapper(){
 */
 def call(Closure body) {
   ansiColor('xterm') {
+    echo "Env before"
+    sh "export"
     withEnv([
       'VAULT_ROLE_ID_HEY_APM=35ad5918-eab7-c814-f8be-a305c811732e', 
       'VAULT_SECRET_ID_HEY_APM=95d18733-44b5-53c3-89c5-91e27b29be4f', 
