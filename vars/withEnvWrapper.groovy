18258e30a1e7062f1b0bdc120f47e8e00f111fe5
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2018-10-16 10:48:09 +0200

    move some stuff to the jenkinsfile

--- vars/withEnvWrapper.groovy
+++ vars/withEnvWrapper.groovy
@@ -6,24 +6,21 @@ withEnvWrapper(){
 }
 */
 def call(Closure body) {
-  ansiColor('xterm') {
-    withEnv([
-      'VAULT_ROLE_ID_HEY_APM=35ad5918-eab7-c814-f8be-a305c811732e', 
-      'VAULT_SECRET_ID_HEY_APM=95d18733-44b5-53c3-89c5-91e27b29be4f', 
-      'VAULT_SECRET_ID=c50f028c-ba5d-a921-7869-c63966e3cd79', 
-      'VAULT_ADDR=https://secrets.elastic.co:8200', 
-      'JOB_GCS_CREDENTIALS=jenkins-gcs-plugin', 
-      'JOB_GCS_BUCKET=apm-ci-artifacts/jobs', 
-      'JOB_GIT_CREDENTIALS=f6c7695a-671e-4f4f-a331-acdce44ff9ba',
-      'NOTIFY_TO=infra-root+build@elastic.co']) {
-        deleteDir()
-        try {
-          body()
-        } catch (err) {
-          echo "${err}"
-          throw err
-        }
-    }
+  withEnv([
+    'VAULT_ROLE_ID_HEY_APM=35ad5918-eab7-c814-f8be-a305c811732e', 
+    'VAULT_SECRET_ID_HEY_APM=95d18733-44b5-53c3-89c5-91e27b29be4f', 
+    'VAULT_SECRET_ID=c50f028c-ba5d-a921-7869-c63966e3cd79', 
+    'VAULT_ADDR=https://secrets.elastic.co:8200', 
+    'JOB_GCS_CREDENTIALS=jenkins-gcs-plugin', 
+    'JOB_GCS_BUCKET=apm-ci-artifacts/jobs',
+    'NOTIFY_TO=infra-root+build@elastic.co']) {
+      deleteDir()
+      try {
+        body()
+      } catch (err) {
+        echo "${err}"
+        throw err
+      }
   }
   /* TODO replace each variable with a secret text credential type, then use withCredentials step.
   https://jenkins.io/doc/book/pipeline/jenkinsfile/#handling-credentials
