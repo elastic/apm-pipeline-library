a84eb26fe6c24cc0c9a9e3f87caa624c5a9c39fa
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2018-11-14 14:05:05 +0100

    add protocol error detection refactor

--- vars/sendBenchmarks.groovy
+++ vars/sendBenchmarks.groovy
@@ -8,7 +8,7 @@
 def call(Map params = [:]) {
   def benchFile = params.containsKey('file') ? params.file : 'bench.out'
   def index = params.containsKey('index') ? params.index : 'benchmark-go'
-  def url = params.containsKey('url') ? params.url : "1ec92c339f616ca43771bff669cc419c.europe-west3.gcp.cloud.es.io:9243"
+  def url = params.containsKey('url') ? params.url : "https://1ec92c339f616ca43771bff669cc419c.europe-west3.gcp.cloud.es.io:9243"
   def secret = params.containsKey('secret') ? params.secret : 'java-agent-benchmark-cloud'
 
   //apm-server-benchmark-cloud
@@ -19,32 +19,44 @@ def call(Map params = [:]) {
   def props = getVaultSecret(secret)
   if(props?.errors){
      error "Benchmarks: Unable to get credentials from the vault: " + props.errors.toString()
+  } 
+  
+  def protocol = "https://"
+  if(url.startsWith("https://")){
+    url = url - "https://"
+    protocol = "https://"
+  } else if (url.startsWith("http://")){
+    echo "withEsEnv: you are using 'http' protocol to access to the service."
+    url = url - "http://"
+    protocol = "http://"
   } else {
-    def data = props?.data
-    def user = data?.user
-    def password = data?.password
-    def urlAuth = "https://${user}:${password}@${url}"
-    
-    if(data == null || user == null || password == null){
-      error "Benchmarks: was not possible to get authentication info to send benchmarks"
-    }
-    echo "Benchmarks: sending data..."
-    wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [
-      [var: 'CLOUD_URL', password: "${urlAuth}"],
-      [var: 'CLOUD_ADDR', password: "${url}"],
-      [var: 'CLOUD_USERNAME', password: "${user}"],
-      [var: 'CLOUD_PASSWORD', password: "${password}"],
-      ]]) {
-         sh """#!/bin/bash
-         set +x -euo pipefail
-         
-         export GOPATH=\${WORKSPACE}
-         export PATH=\${GOPATH}/bin:\${PATH}
-         eval "\$(gvm \${GO_VERSION})"
-         
-         go get -v -u github.com/elastic/gobench
-         gobench -index ${index} -es "${urlAuth}" < ${benchFile}
-         """
-     }
+    error "Benchmarks: unknow protocol, the url is not http(s)."
   }
+  
+  def data = props?.data
+  def user = data?.user
+  def password = data?.password
+  def urlAuth = "${protocol}${user}:${password}@${url}"
+  
+  if(data == null || user == null || password == null){
+    error "Benchmarks: was not possible to get authentication info to send benchmarks"
+  }
+  echo "Benchmarks: sending data..."
+  wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [
+    [var: 'CLOUD_URL', password: "${urlAuth}"],
+    [var: 'CLOUD_ADDR', password: "${protocol}${url}"],
+    [var: 'CLOUD_USERNAME', password: "${user}"],
+    [var: 'CLOUD_PASSWORD', password: "${password}"],
+    ]]) {
+       sh """#!/bin/bash
+       set +x -euo pipefail
+       
+       export GOPATH=\${WORKSPACE}
+       export PATH=\${GOPATH}/bin:\${PATH}
+       eval "\$(gvm \${GO_VERSION})"
+       
+       go get -v -u github.com/elastic/gobench
+       gobench -index ${index} -es "${urlAuth}" < ${benchFile}
+       """
+   }
 }
