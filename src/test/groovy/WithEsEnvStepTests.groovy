659ac8f4da1224ef3a79ac87366db859411104b7
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2018-11-14 14:03:51 +0100

    new step withEsEnv

---
+++ src/test/groovy/WithEsEnvStepTests.groovy
@@ -0,0 +1,147 @@
+import com.lesfurets.jenkins.unit.BasePipelineTest
+import org.junit.Before;
+import org.junit.Test;
+import static com.lesfurets.jenkins.unit.MethodCall.callArgsToString
+import static org.junit.Assert.assertTrue
+
+class WithEsEnvStepTests extends BasePipelineTest {
+  Map env = [:]
+    
+  def wrapInterceptor = { map, closure ->
+    map.each { key, value -> 
+      if("varPasswordPairs".equals(key)){
+        value.each{ it ->
+          binding.setVariable("${it.var}", "${it.password}")
+        }
+      }
+    }
+    def res = closure.call()
+    map.forEach { key, value ->
+      if("varPasswordPairs".equals(key)){
+        value.each{ it ->
+          binding.setVariable("${it.var}", null)
+        }
+      }
+    }
+    return res
+  }
+  
+  @Override
+  @Before
+  void setUp() throws Exception {
+    super.setUp()
+    
+    env.BRANCH_NAME = "branch"
+    env.CHANGE_ID = "29480a51"
+    env.ORG_NAME = "org"
+    env.REPO_NAME = "repo"
+    env.GITHUB_TOKEN = "TOKEN"
+    binding.setVariable('env', env)
+
+    helper.registerAllowedMethod("sh", [Map.class], { "OK" })
+    helper.registerAllowedMethod("sh", [String.class], { "OK" })
+    helper.registerAllowedMethod("wrap", [Map.class, Closure.class], wrapInterceptor)
+    helper.registerAllowedMethod("error", [String.class], { s ->
+      updateBuildStatus('FAILURE')
+      throw new Exception(s)
+    })
+    helper.registerAllowedMethod("getVaultSecret", [String.class], { s ->
+      if("secret".equals(s) || "java-agent-benchmark-cloud".equals(s)){
+        return [data: [ user: 'username', password: 'user_password']] 
+      }
+      if("secretError".equals(s)){
+        return [errors: 'Error message'] 
+      }
+      if("secretNotValid".equals(s)){
+        return [data: [ user: null, password: null]] 
+      }
+      return null
+    })
+  }
+  
+  @Test
+  void test() throws Exception {
+    def script = loadScript("vars/withEsEnv.groovy")
+    def isOK = false
+    script.call(secret: 'secret'){
+      isOK = true
+    }
+    printCallStack()
+    assertTrue(isOK)
+    assertJobStatusSuccess()
+  }
+  
+  @Test
+  void testParams() throws Exception {
+    def script = loadScript("vars/withEsEnv.groovy")
+    def isOK = false
+    script.call(url: 'https://es.example.com', secret: 'secret'){
+      if(binding.getVariable("CLOUD_URL") == "https://username:user_password@es.example.com"
+      && binding.getVariable("CLOUD_ADDR") == "https://es.example.com"
+      && binding.getVariable("CLOUD_USERNAME") == "username"
+      && binding.getVariable("CLOUD_PASSWORD") == "user_password"){
+        isOK = true
+      }
+    }
+    printCallStack()
+    assertTrue(isOK)
+    assertJobStatusSuccess()
+  }
+  
+  @Test
+  void testSecretNotFound() throws Exception {
+    def script = loadScript("vars/withEsEnv.groovy")
+    try{
+      script.call(secret: 'secretNotExists'){
+        //NOOP
+      }
+    } catch(e){
+      //NOOP
+    }
+    printCallStack()
+    assertTrue(helper.callStack.findAll { call ->
+        call.methodName == "error"
+    }.any { call ->
+        callArgsToString(call).contains("withEsEnv: was not possible to get authentication info")
+    })
+    assertJobStatusFailure()
+  }
+  
+  @Test
+  void testSecretError() throws Exception {
+    def script = loadScript("vars/withEsEnv.groovy")
+    try {
+      script.call(secret: 'secretError'){
+        //NOOP
+      }
+    } catch(e){
+      //NOOP
+    }
+    printCallStack()
+    assertTrue(helper.callStack.findAll { call ->
+        call.methodName == "error"
+    }.any { call ->
+        callArgsToString(call).contains("withEsEnv: Unable to get credentials from the vault: Error message")
+    })
+    assertJobStatusFailure()
+  }
+  
+  @Test
+  void testWrongProtocol() throws Exception {
+    def script = loadScript("vars/withEsEnv.groovy")
+    try {
+      script.call(secret: 'secret', url: 'ht://wrong.example.com'){
+        //NOOP
+      }
+    } catch(e){
+      //NOOP
+    }
+    printCallStack()
+    assertTrue(helper.callStack.findAll { call ->
+        call.methodName == "error"
+    }.any { call ->
+        callArgsToString(call).contains("withEsEnv: unknow protocol, the url is not http(s).")
+    })
+    assertJobStatusFailure()
+  }
+}
---
+++ vars/withEsEnv.groovy
@@ -0,0 +1,52 @@
+#!/usr/bin/env groovy
+
+/**
+  Grab a secret from the vault and define some environment variables to access to an URL
+
+  withEsEnv(){
+    //block
+  }
+
+  withEsEnv(url: 'https://url.exanple.com', secret: 'secret-name'){
+    //block
+  }
+*/
+def call(Map params = [:], Closure body) {
+  def url = params.containsKey('url') ? params.url : 'https://5492443829134f71a94c96689e9db66e.europe-west3.gcp.cloud.es.io:9243'
+  def secret = params?.secret
+  
+  def props = getVaultSecret(secret)
+  if(props?.errors){
+     error "withEsEnv: Unable to get credentials from the vault: " + props.errors.toString()
+  }
+  
+  def protocol = "https://"
+  if(url.startsWith("https://")){
+    url = url - "https://"
+    protocol = "https://"
+  } else if (url.startsWith("http://")){
+    echo "withEsEnv: you are using 'http' protocol to access to the service."
+    url = url - "http://"
+    protocol = "http://"
+  } else {
+    error "withEsEnv: unknow protocol, the url is not http(s)."
+  }
+  
+  def data = props?.data
+  def user = data?.user
+  def password = data?.password
+  def urlAuth = "${protocol}${user}:${password}@${url}"
+  
+  if(data == null || user == null || password == null){
+    error "withEsEnv: was not possible to get authentication info"
+  }
+  
+  wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [
+    [var: 'CLOUD_URL', password: "${urlAuth}"],
+    [var: 'CLOUD_ADDR', password: "${protocol}${url}"],
+    [var: 'CLOUD_USERNAME', password: "${user}"],
+    [var: 'CLOUD_PASSWORD', password: "${password}"],
+    ]]) {
+      body()
+   }
+}
