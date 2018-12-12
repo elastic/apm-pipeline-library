f7116a54485403c40deecd3a76b493837ee6ca08
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2018-12-12 12:14:33 +0100

    stop container to make a test

--- vars/pipelineApmUI.groovy
+++ vars/pipelineApmUI.groovy
@@ -307,35 +307,34 @@ def kibanaGroupSteps(){
     //unstash 'cache'
     unstash 'build-oss'
     checkoutSteps()
+    checkoutES()
     dir("${BASE_DIR}"){
-      script {
-        def parallelSteps = [:]
-        def groups = (1..12)
-        
-        parallelSteps['ensureAllTestsInCiGroup'] = {sh '''#!/bin/bash
+      def parallelSteps = [:]
+      def groups = (1..12)
+      input("Can we continue?")
+      
+      sh '''#!/bin/bash
+      set -euxo pipefail
+      PATH=${PATH}:$(yarn bin)
+      grunt functionalTests:ensureAllTestsInCiGroup || echo -e "\033[31;49mTests FAILED\033[0m"
+      '''
+      
+      parallelSteps['pluginFunctionalTestsRelease'] = {sh '''#!/bin/bash
+      set -euxo pipefail
+      PATH=${PATH}:$(yarn bin)
+      
+      grunt run:pluginFunctionalTestsRelease --from=source || echo -e "\033[31;49mTests FAILED\033[0m"
+      '''}
+      
+      groups.each{ group ->
+        parallelSteps["functionalTests_ciGroup${group}"] ={sh """#!/bin/bash
         set -euxo pipefail
-        PATH=${PATH}:$(yarn bin)
+        PATH=\${PATH}:\$(yarn bin)
         
-        grunt functionalTests:ensureAllTestsInCiGroup || echo -e "\033[31;49mTests FAILED\033[0m"
-        '''}
-        
-        parallelSteps['pluginFunctionalTestsRelease'] = {sh '''#!/bin/bash
-        set -euxo pipefail
-        PATH=${PATH}:$(yarn bin)
-        
-        grunt run:pluginFunctionalTestsRelease --from=source || echo -e "\033[31;49mTests FAILED\033[0m"
-        '''}
-        
-        groups.each{ group ->
-          parallelSteps["functionalTests_ciGroup${group}"] ={sh """#!/bin/bash
-          set -euxo pipefail
-          PATH=\${PATH}:\$(yarn bin)
-          
-          grunt "run:functionalTests_ciGroup${group}" --from=source || echo -e "\033[31;49mTests FAILED\033[0m"
-          """}
-        }
-        parallel(parallelSteps)
+        grunt "run:functionalTests_ciGroup${group}" --from=source || echo -e "\033[31;49mTests FAILED\033[0m"
+        """}
       }
+      parallel(parallelSteps)
     }
   }
 }
