7fe91ded19e0a8efdbe376705bb0e6b1bba56f90
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2018-12-18 20:11:36 +0100

    refactor

--- vars/githubPrInfo.groovy
+++ vars/githubPrInfo.groovy
@@ -2,8 +2,8 @@
 TODO make test
 */
 def call(Map params = [:]){  
-  def token =  params.containsKey('token') ? params.token
-  def repo =  params.containsKey('repo') ? params.url
+  def token =  params?.token
+  def repo =  params?.url
   def pr =  params.containsKey('pr') ? params.pr : error('githubPrInfo: no valid PR ID.')
   return githubApiCall(token, "https://api.github.com/repos/${repo}/pulls/${pr}")
 }
