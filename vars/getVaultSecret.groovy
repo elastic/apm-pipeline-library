8f5e91c516a250728224771c70932eda48694874
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2019-01-03 14:28:24 +0100

    remove the use of jq

--- vars/getVaultSecret.groovy
+++ vars/getVaultSecret.groovy
@@ -8,19 +8,32 @@ def call(secret) {
   def roleId = '35ad5918-eab7-c814-f8be-a305c811732e'
   def secretId = '95d18733-44b5-53c3-89c5-91e27b29be4f'
   def addr = 'https://secrets.elastic.co:8200'
-  log(level: 'INFO', text: "Getting secrets")
+  log(level: 'INFO', text: "getVaultSecret: Getting secrets")
   wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [
     [var: 'VAULT_ROLE_ID', password: roleId], 
     [var: 'VAULT_SECRET_ID', password: secretId], 
     [var: 'VAULT_ADDR', password: addr], 
     [var: 'VAULT_SECRET', password: "${secret}"], 
     ]]) {
-      def retJson = sh(returnStdout: true, script: """#!/bin/bash
+      def tokenJson =  sh(returnStdout: true, script: """#!/bin/bash
       set +x -euo pipefail
-      VAULT_TOKEN=\$(curl -s -X POST -H "Content-Type: application/json" -L -d '{"role_id":"${roleId}","secret_id":"${secretId}"}' ${addr}/v1/auth/approle/login | jq -r '.auth.client_token' )
-      curl -s -L -H "X-Vault-Token:\${VAULT_TOKEN}" ${addr}/v1/secret/apm-team/ci/${secret}
+      curl -s -X POST -H "Content-Type: application/json" -L -d '{"role_id":"${roleId}","secret_id":"${secretId}"}' ${addr}/v1/auth/approle/login
       """)
-      props = readJSON(text: retJson)
+      def token = readJSON(text: tokenJson)?.auth?.client_token
+      
+      if(token == null){
+        error("getVaultSecret: Unable to get the token.")
+      }
+      
+      wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [
+      [var: 'VAULT_TOKEN', password: token],
+      ]]) {
+        def retJson = sh(returnStdout: true, script: """#!/bin/bash
+        set +x -euo pipefail
+        curl -s -L -H "X-Vault-Token:${token}" ${addr}/v1/secret/apm-team/ci/${secret}
+        """)
+        props = readJSON(text: retJson)
+      }
    }
    return props
 }
