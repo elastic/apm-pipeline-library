5b94405efe80c0a51cf977c2ffbfd225b8896c38
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2019-01-15 15:45:54 +0100

    Parallel tasks generator class

---
+++ src/co/elastic/axis/DefaultParallelTaskGenerator.groovy
@@ -0,0 +1,155 @@
+package co.elastic
+
+/**
+  Base class to generate parallel tasks from a series of YAML files.
+*/
+class DefaultParallelTaskGenerator {
+  /** Attribute to store results */
+  public Map results = [:]
+  /** Tag to identify the tasks */
+  public String tag
+  /** YAML file 'x' coordinates Key */
+  public String xKey
+  /** YAML file 'y' coordinates Key */
+  public String yKey
+  /** YAML file 'x' coordinates path */
+  public String xFile
+  /** YAML file 'y' coordinates path */
+  public String yFile
+  /** YAML file exclusions path */
+  public String exclusionFile
+  /** Name to be displayed in the UI/logs */
+  public String name
+  /** object to access to pipeline steps */
+  public steps
+  
+  public DefaultParallelTaskGenerator(Map params){
+    this.name = params.name
+    this.tag = params.tag
+    this.xKey = params.xKey
+    this.yKey = params.yKey
+    this.xFile = params.xFile
+    this.yFile = params.yFile
+    this.exclusionFile = params.exclusionFile
+    this.steps = params.steps
+  }
+  /**
+    read the X versions YAML file and return a versions list.
+  */
+  protected Map getXVersions(){
+    return steps.readYaml(file: this.xFile)[this.xKey]
+  }
+
+  /**
+    read the Y versions YAML file and return a versions list.
+  */
+  protected Map getYVersions(){
+    return steps.readYaml(file: this.yFile)[this.yKey]
+  }
+
+  /**
+    read the excludes YAML file and return a map of version pairs to exclude.
+  */
+  protected List getExcludeVersions(){
+    def ret = []
+    steps.readYaml(file: this.exclusionFile)['exclude'].each{ v ->
+      def x = v[this.xKey]
+      def y = v[this.yKey]
+      String key = "${x}#${y}"
+      steps.log(level: "DEBUG", text: "Exclude : ${key}")
+      ret.add(key)
+    }
+    return ret
+  }
+
+  /**
+    save a test exit record to the results variable.
+  */
+  protected saveResult(x, y, value){
+    if(results.data[x] == null){
+      results.data[x] = [:]
+      results.data[x]["Agent ${results.name}"] = x
+    }
+    results.data[x][y] = value
+  }
+
+  /**
+    build a map pairs of a x value with every 'y' value, and remove the excludes.
+  */
+  protected Map buildColumn(x, yItems, excludes){
+    def column = [:]
+    yItems.each{ y ->
+      String key = "${x}#${y}"
+      if(!excludes.contains(key)){
+        column[key] = [X: x, Y: y]
+      }
+    }
+    return column
+  }
+
+  /**
+    Initialize the results map, read the info from the YAML files, and call the 
+    buildMatrix method to build the parallel tasks.
+  */
+  public Map generateParallelTests() {
+    results = [:]
+    results.data = [:]
+    results.tag = this.tag
+    results.name = this.name
+    results.x = getXVersions()
+    results.y = getYVersions()
+    results.excludes = getExcludeVersions()
+    return buildMatrix();
+  }
+
+  /**
+    build the x,y pairs, remove the excludes and call the method 
+    to build the parallel task for each pair.
+  */
+  protected Map buildMatrix(){
+    def parallelSteps = [:]
+    results.x.each{ x ->
+      def column = buildColumn(x, results.y, results.excludes)
+      def stagesMap = generateParallelSteps(column)
+      parallelSteps.putAll(stagesMap)
+    }
+    steps.log(level: "DEBUG", text: "parallelStages : ${parallelSteps.toString()}")
+    return parallelSteps
+  }
+
+  /**
+    build a map of clousures to be used as parallel steps.
+    This method can be overwritten by the target pipeline.
+  */
+  protected Map generateParallelSteps(column){
+    def parallelStep = [:]
+    column.each{ key, value ->
+      def keyGrp = "${this.tag}-${value.X}-${value.Y}"
+      parallelStep[keyGrp] = generateStep(value.X, value.Y)
+    }
+    return parallelStep
+  }
+  
+  /**
+    Default step implementation, it will provision a node and echo some text.
+    This method should be overwritten by the target pipeline.
+  */
+  protected Closure generateStep(x, y){
+    return {
+      steps.node('linux && immutable'){
+        try {
+          steps.echo "${tag} - ${x} - ${y}"
+          saveResult(x, y, 1)
+        } catch(e){
+          saveResult(x, y, 0)
+          steps.error("Some ${tag} tests failed")
+        } finally {
+          steps.junit(
+            allowEmptyResults: false, 
+            keepLongStdio: true, 
+            testResults: "**/tests/results/*-junit*.xml")
+        }
+      }
+    }
+  }
+}
---
+++ src/co/elastic/axis/ResultsProcessor.groovy
@@ -0,0 +1,118 @@
+package co.elastic
+
+/**
+  Process the result map variable and build and html report.
+*/
+public processResults(results){
+  sh 'curl -sLO https://code.jquery.com/jquery-3.3.1.slim.min.js'
+  sh 'curl -sLO https://cdn.jsdelivr.net/npm/htmlson.js@1.0.4/src/htmlson.js'
+  def jquery = readFile(file: 'jquery-3.3.1.slim.min.js')
+  def htmlson = readFile(file: 'htmlson.js')
+  String html = """<html>
+  <head>
+    <title>Integration Test Results</title>
+  </head>
+  <script type="text/javascript">${jquery}</script>
+  <script type="text/javascript">${htmlson}</script>
+  <style>
+    table {
+      font-family: Arial, Helvetica, sans-serif;
+      border: 1px solid #000000;
+      background-color: #EEEEEE;
+      width: 50em;
+      text-align: center;
+      border-collapse: collapse;
+      margin: 30px 20px;
+    }
+    table td, table th {
+      border: 1px solid #AAAAAA;
+      padding: 3px 2px;
+    }
+    table tbody td {
+      font-size: 13px;
+    }
+    table tr:nth-child(even) {
+      background: #D0E4F5;
+    }
+    table thead {
+      background: #1C6EA4;
+      background: -moz-linear-gradient(top, #5592bb 0%, #327cad 66%, #1C6EA4 100%);
+      background: -webkit-linear-gradient(top, #5592bb 0%, #327cad 66%, #1C6EA4 100%);
+      background: linear-gradient(to bottom, #5592bb 0%, #327cad 66%, #1C6EA4 100%);
+      border-bottom: 2px solid #444444;
+    }
+    table thead th {
+      font-size: 15px;
+      font-weight: bold;
+      color: #FFFFFF;
+      text-align: center;
+      border-left: 2px solid #D0E4F5;
+    }
+    table thead th:first-child {
+      border-left: none;
+    }
+
+    table tfoot td {
+      font-size: 14px;
+    }
+    table tfoot .links {
+      text-align: right;
+    }
+    table tfoot .links a{
+      display: inline-block;
+      background: #1C6EA4;
+      color: #FFFFFF;
+      padding: 2px 8px;
+      border-radius: 5px;
+    }
+    
+    .error {
+      color: red;
+    }
+    
+    .ok {
+      color: green;
+    }
+  </style>
+  <body>
+  """
+  
+  results.each{ k, v ->
+    def records = []
+    v.data.each{ dk, dv ->
+      def row = [:]
+      v.y.each{ vy ->
+        row.put(vy, "N/A")
+      }
+      row.putAll(dv)
+      records.add(row)
+    }
+    
+    String jsonRecords = toJSON(records).toString()
+    html += """
+    <h2>Agent ${v.name}</h2>
+    <table class="${k}Agent"></table>
+    <script type="text/javascript">
+      let ${k}Data = ${jsonRecords};
+      let ${k}Table = \$('.${k}Agent').htmlson({data: ${k}Data});
+    </script>
+    """
+  }
+  
+  html += '''
+  <script type="text/javascript">
+    $('td').each(function(){
+      if(this.textContent === "1"){
+        $( this ).replaceWith("<td class='ok'>OK</td>");
+      } else if(this.textContent === "0") {
+        $( this ).replaceWith("<td class='error'>ERROR</td>");
+      }
+    });
+  </script>
+  </body>
+  </html>
+  '''
+  writeFile(file: 'results.html', text: html)
+}
+
+return this
