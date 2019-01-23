685722b9af53aa53c211426a0a425c553deebea8
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2019-01-23 17:18:43 +0100

    base64encoding step

--- vars/base64encode.groovy
+++ vars/base64encode.groovy
@@ -1,7 +1,7 @@
 /**
   base64encode(text: "text to encode", encoding: "UTF-8")
 */
-call(Map params = [:]){
+def call(Map params = [:]){
   def text = params.text
   def encoding = params.containsKey('encoding') ? params.encoding : "UTF-8"
 
