b77b201a3b7f12e1447e62cd2b606a36fd947da6
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2019-03-19 17:16:49 +0100

    mend

--- vars/isCommentTrigger.groovy
+++ vars/isCommentTrigger.groovy
@@ -4,12 +4,15 @@
   def commentTrigger = isCommentTrigger()
 */
 def call(){
-  def ret = currentBuild.getBuildCauses()?.any{ it._class.startsWith('org.jenkinsci.plugins.pipeline.github.trigger.IssueCommentCause')}
+  def triggerCause = currentBuild.rawBuild.getCause(org.jenkinsci.plugins.pipeline.github.trigger.IssueCommentCause)
+
+  def ret = triggerCause != null
+  //currentBuild.getBuildCauses()?.any{ it._class.startsWith('org.jenkinsci.plugins.pipeline.github.trigger.IssueCommentCause')}
   log(level: 'DEBUG', text: "isCommentTrigger: ${ret}")
   if(ret){
-    def buildCause = currentBuild.getBuildCauses().find{ it._class.startsWith('org.jenkinsci.plugins.pipeline.github.trigger.IssueCommentCause')}
-    log(level: 'DEBUG', text: "isCommentTrigger: ${buildCause}")
-    env.BUILD_CAUSE_USER = buildCause.getUserLogin()
+    //def buildCause = currentBuild.getBuildCauses().find{ it._class.startsWith('org.jenkinsci.plugins.pipeline.github.trigger.IssueCommentCause')}
+    log(level: 'DEBUG', text: "isCommentTrigger: ${triggerCause}")
+    env.BUILD_CAUSE_USER = triggerCause.getUserLogin()
     //Only Elastic users are allowed
     def token = getGithubToken()
     def user = githubApiCall(token: token, url: "https://api.github.com/users/${env.BUILD_CAUSE_USER}")
