98abb8e599025056fd1218a74f9dc3aecb60ec69
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2019-03-19 17:13:23 +0100

    use getUserLogin()

--- vars/isCommentTrigger.groovy
+++ vars/isCommentTrigger.groovy
@@ -9,7 +9,7 @@ def call(){
   if(ret){
     def buildCause = currentBuild.getBuildCauses().find{ it._class.startsWith('org.jenkinsci.plugins.pipeline.github.trigger.IssueCommentCause')}
     log(level: 'DEBUG', text: "isCommentTrigger: ${buildCause}")
-    env.BUILD_CAUSE_USER = buildCause?.userLogin
+    env.BUILD_CAUSE_USER = buildCause.getUserLogin()
     //Only Elastic users are allowed
     def token = getGithubToken()
     def user = githubApiCall(token: token, url: "https://api.github.com/users/${env.BUILD_CAUSE_USER}")
