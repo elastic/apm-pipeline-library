3efbe6efca93b4fe0c4e53ac3cfb989f725f4910
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2019-04-04 18:04:22 +0200

    debug

--- vars/dockerLogin.groovy
+++ vars/dockerLogin.groovy
@@ -9,14 +9,15 @@ def call(Map params = [:]){
   def secret = params.containsKey('secret') ? params.secret : error("dockerLogin: No valid secret to looking for.")
   def registry = params.containsKey('registry') ? params.registry : "docker.io"
   def jsonValue = getVaultSecret(secret: secret)
-
+  log(level: "DEBUG", text: "secret: ${jsonValue.toString()}")
   def user = jsonValue.data.user =! null ? jsonValue.data.user : error("dockerLogin: No valid user in secret.")
   def password = jsonValue.data.password =! null ? jsonValue.data.password : error("dockerLogin: No valid password in secret.")
-
+  log(level: "DEBUG", text: "auth: ${user} ${password}")
   wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [
     [var: 'DOCKER_USER', password: user],
     [var: 'DOCKER_PASSWORD', password: password],
     ]]) {
+    log(level: "DEBUG", text: "auth: ${user} ${password}")
     sh(label: "Docker login", script: "docker login -u ${user} -p ${password} ${registry}")
   }
 }
