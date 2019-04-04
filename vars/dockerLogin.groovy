dacd6a980b4a53b09d85e6a5ff525c5d62e356fc
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2019-04-04 18:21:45 +0200

    debug

--- vars/dockerLogin.groovy
+++ vars/dockerLogin.groovy
@@ -10,14 +10,14 @@ def call(Map params = [:]){
   def registry = params.containsKey('registry') ? params.registry : "docker.io"
   def jsonValue = getVaultSecret(secret: secret)
   log(level: "DEBUG", text: "secret: ${jsonValue.data.toString()}")
-  def user = jsonValue.data.user =! null ? jsonValue.data.user : error("dockerLogin: No valid user in secret.")
-  def password = jsonValue.data.password =! null ? jsonValue.data.password : error("dockerLogin: No valid password in secret.")
+  def dockerUser = jsonValue.data.user =! null ? jsonValue.data.user : error("dockerLogin: No valid user in secret.")
+  def dockerPassword = jsonValue.data.password =! null ? jsonValue.data.password : error("dockerLogin: No valid password in secret.")
   log(level: "DEBUG", text: "auth: ${jsonValue.data.user} ${jsonValue.data.password}")
   wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [
-    [var: 'DOCKER_USER', password: user],
-    [var: 'DOCKER_PASSWORD', password: password],
+    [var: 'DOCKER_USER', password: dockerUser],
+    [var: 'DOCKER_PASSWORD', password: dockerPassword],
     ]]) {
-    log(level: "DEBUG", text: "auth: ${user} ${password}")
-    sh(label: "Docker login", script: "docker login -u ${user} -p ${password} ${registry}")
+    log(level: "DEBUG", text: "auth: ${dockerUser} ${dockerPassword}")
+    sh(label: "Docker login", script: "docker login -u ${dockerUser} -p ${dockerPassword} ${registry}")
   }
 }
