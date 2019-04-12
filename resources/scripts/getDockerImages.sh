5e6be264f177549deef4b6dda81cb19d0bbe064e
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2019-04-12 17:18:55 +0200

    update getDockerImages.sh

--- resources/scripts/getDockerImages.sh
+++ resources/scripts/getDockerImages.sh
@@ -6,26 +6,27 @@ if [ $# -lt 1 ]; then
   exit 1
 fi
 
-VERSION=${1}
+VERSION_ID=${1}
 
 #VERSION=7.0
 #https://artifacts-api.elastic.co/v1/versions/${VERSION}/builds/latest
-JSON=$(curl https://staging.elastic.co/latest/${VERSION}.json)
-BUILD_ID=$(echo ${JSON}|jq -r .build_id)
-VERSION_ID=$(echo ${JSON}|jq -r .version)
-MANIFEST_URL=$(echo ${JSON}|jq -r .manifest_url)
-MANIFEST_JSON=$(curl ${MANIFEST_URL})
+#JSON=$(curl https://staging.elastic.co/latest/${VERSION}.json)
+#BUILD_ID=$(echo ${JSON}|jq -r .build_id)
+#VERSION_ID=$(echo ${JSON}|jq -r .version)
+#MANIFEST_URL=$(echo ${JSON}|jq -r .manifest_url)
+MANIFEST_JSON=$(curl -sSf https://artifacts-api.elastic.co/v1/versions/${VERSION_ID}/builds/latest/)
+#VERSION_ID=$(echo ${MANIFEST_JSON}|jq -r .version)
 
 for product in elasticsearch kibana apm-server
 do
-  URL=$(echo ${MANIFEST_JSON}|jq -r ".projects.\"${product}\".packages[]|select(.type==\"docker\").url"|grep "${product}-${VERSION_ID}")
+  URL=$(echo ${MANIFEST_JSON}|jq -r ".build.projects.\"${product}\".packages[]|select(.type==\"docker\").url"|grep "${product}-${VERSION_ID}")
   echo "Downloading ${product} - ${URL}"
   curl ${URL}|docker load
 done
 
 for product in auditbeat filebeat heartbeat metricbeat packetbeat
 do
-  URL=$(echo ${MANIFEST_JSON}|jq -r ".projects.beats.packages[]|select(.type==\"docker\").url"|grep "${product}-${VERSION_ID}")
+  URL=$(echo ${MANIFEST_JSON}|jq -r ".build.projects.beats.packages[]|select(.type==\"docker\").url"|grep "${product}-${VERSION_ID}")
   echo "Downloading ${product} - ${URL}"
   curl ${URL}|docker load
 done
