b5b658b3ffe46b44be6ee4eb13a034a4a048c915
Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>
2019-04-16 13:43:49 +0200

    Update buildDockerImages.groovy

--- resources/buildDockerImages.groovy
+++ resources/buildDockerImages.groovy
@@ -49,10 +49,8 @@ pipeline {
         script{
           dockerLogin(secret: "${DOCKERHUB_SECRET}", registry: 'docker.io')
           sh(label: 'pull Docker image', script: "docker pull store/oracle/weblogic:12.2.1.3-dev")
-          
-          env.HOME = "${env.JENKINS_HOME}"
-          sh "docker login ${params.registry}"
-          
+          sh "export"
+          sh "ls -la ${JENKINS_HOME}"
           if(params.secret != null && "${params.secret}" != ""){
              dockerLogin(secret: "${params.secret}", registry: "${params.registry}")
           }
