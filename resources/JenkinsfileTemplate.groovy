9ca4860e7852912594e101d18d04f9bda9d9bf1b
Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>
2019-05-23 16:13:42 +0200

    Update JenkinsfileTemplate.groovy

--- resources/JenkinsfileTemplate.groovy
+++ resources/JenkinsfileTemplate.groovy
@@ -41,20 +41,6 @@ pipeline {
         PARAM_WITH_DEFAULT_VALUE = "${params?.PARAM_WITH_DEFAULT_VALUE}"
       }
       stages {
-        stage('Check flyweight Commands support'){
-          steps{
-            sh returnStatus: true, script: 'docker --version'
-            sh returnStatus: true, script: 'java -version'
-            sh returnStatus: true, script: '''
-            JAVA_HOME="${HUDSON_HOME}/.java/java10"
-            PATH="${JAVA_HOME}/bin:${PATH}"
-            java -version
-            '''
-            sh returnStatus: true, script: 'go version'
-            sh returnStatus: true, script: 'git version'
-            sh returnStatus: true, script: 'mvn --version'
-          }
-        }
         /**
         Checkout the code and stash it, to use it on other stages.
         */
