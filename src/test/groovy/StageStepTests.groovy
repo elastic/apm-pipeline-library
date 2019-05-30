033897ecf9318c4b09cb56131848de4726c67feb
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-05-30 13:04:33 +0100

    [APM-CI] Override built-in step to extend the GH notify functionality

---
+++ src/test/groovy/StageStepTests.groovy
@@ -0,0 +1,72 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+import com.lesfurets.jenkins.unit.BasePipelineTest
+import org.junit.Before
+import org.junit.Test
+import static com.lesfurets.jenkins.unit.MethodCall.callArgsToString
+import static org.junit.Assert.assertTrue
+
+class StageStepTests extends BasePipelineTest {
+  String scriptName = "vars/stage.groovy"
+  Map env = [:]
+
+  @Override
+  @Before
+  void setUp() throws Exception {
+    super.setUp()
+
+    env.WORKSPACE = "WS"
+    env.JOB_NAME = "testJob"
+    env.JENKINS_URL = "http://jenkins.example.com:8080"
+    env.JOB_URL = "${env.JENKINS_URL}/job/${env.JOB_NAME}"
+
+    helper.registerAllowedMethod("error", [String.class], { s ->
+      updateBuildStatus('FAILURE')
+      throw new Exception(s)
+    })
+    helper.registerAllowedMethod("stage", [String.class, Closure.class], { body -> body() })
+    helper.registerAllowedMethod("stage", [String.class, String.class, Closure.class], { body -> body() })
+    helper.registerAllowedMethod("githubNotify", [Map.class], { "OK" })
+  }
+
+  @Test
+  void testMissingArguments() throws Exception {
+    def script = loadScript(scriptName)
+    try {
+      script.call(){
+        //NOOP
+      }
+    } catch(e){
+      //NOOP
+    }
+    printCallStack()
+    assertJobStatusFailure()
+  }
+
+  @Test
+  void test() throws Exception {
+    def script = loadScript(scriptName)
+    def isOK = false
+    script.call('name', 'foo'){
+      isOK = true
+    }
+    printCallStack()
+    assertTrue(isOK)
+    assertJobStatusSuccess()
+  }
+}
