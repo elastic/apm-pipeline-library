bde835c0330be401a77f443c9ff38abcfd886078
Victor Martinez <victormartinezrubio@gmail.com>
2019-06-12 12:25:27 +0100

    [APM-CI] checkLicenses step (#76)

---
+++ src/test/groovy/CheckLicensesStepTests.groovy
@@ -0,0 +1,129 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+import com.lesfurets.jenkins.unit.BasePipelineTest
+import org.junit.Before
+import org.junit.Test
+import static com.lesfurets.jenkins.unit.MethodCall.callArgsToString
+import static org.junit.Assert.assertTrue
+
+class CheckLicensesStepTests extends BasePipelineTest {
+  static final String scriptName = 'vars/checkLicenses.groovy'
+  Map env = [:]
+
+  /**
+   * Mock Docker class from docker-workflow plugin.
+   */
+  class Docker implements Serializable {
+
+    public Image image(String id) {
+      new Image(this, id)
+    }
+
+    public class Image implements Serializable {
+      private Image(Docker docker, String id) {}
+      public <V> V inside(String args = '', Closure<V> body) { body() }
+    }
+  }
+
+  @Override
+  @Before
+  void setUp() throws Exception {
+    super.setUp()
+
+    env.WORKSPACE = '/tmp'
+    env.BASE_DIR = 'base'
+
+    binding.setVariable('env', env)
+    binding.setProperty('docker', new Docker())
+
+    helper.registerAllowedMethod('sh', [Map.class], { 'OK' })
+  }
+
+  @Test
+  void testSuccessWithoutArgument() throws Exception {
+    def script = loadScript(scriptName)
+    script.call()
+    printCallStack()
+    assertJobStatusSuccess()
+  }
+
+  @Test
+  void testSuccessWithExtArgument() throws Exception {
+    def script = loadScript(scriptName)
+    script.call(ext: '.foo')
+    printCallStack()
+    assertJobStatusSuccess()
+    assertTrue(helper.callStack.findAll { call ->
+      call.methodName == 'sh'
+    }.any { call ->
+      callArgsToString(call).contains('-ext .foo')
+    })
+  }
+
+  @Test
+  void testSuccessWithExcludeArgument() throws Exception {
+    def script = loadScript(scriptName)
+    script.call(exclude: './bar')
+    printCallStack()
+    assertJobStatusSuccess()
+    assertTrue(helper.callStack.findAll { call ->
+      call.methodName == 'sh'
+    }.any { call ->
+      callArgsToString(call).contains('-exclude ./bar')
+    })
+  }
+
+  @Test
+  void testSuccessWithLicenseArgument() throws Exception {
+    def script = loadScript(scriptName)
+    script.call(license: 'Elastic')
+    printCallStack()
+    assertJobStatusSuccess()
+    assertTrue(helper.callStack.findAll { call ->
+      call.methodName == 'sh'
+    }.any { call ->
+      callArgsToString(call).contains('-license Elastic')
+    })
+  }
+
+  @Test
+  void testSuccessWithLicensorArgument() throws Exception {
+    def script = loadScript(scriptName)
+    script.call(licensor: 'Foo S.A.')
+    printCallStack()
+    assertJobStatusSuccess()
+    assertTrue(helper.callStack.findAll { call ->
+      call.methodName == 'sh'
+    }.any { call ->
+      callArgsToString(call).contains('-licensor "Foo S.A."')
+    })
+  }
+
+  @Test
+  void testSuccessWithSkipArgument() throws Exception {
+    def script = loadScript(scriptName)
+    script.call(skip: true)
+    printCallStack()
+    assertJobStatusSuccess()
+    assertTrue(helper.callStack.findAll { call ->
+      call.methodName == 'sh'
+    }.any { call ->
+      callArgsToString(call).contains('-d')
+    })
+  }
+}
---
+++ vars/checkLicenses.groovy
@@ -0,0 +1,42 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+/**
+  Use the elastic licenser
+
+  checkLicenses()
+
+  checkLicenses(ext: '.groovy')
+
+  checkLicenses(skip: true, ext: '.groovy')
+
+  checkLicenses(ext: '.groovy', exclude: './target', license: 'Elastic', licensor: 'Elastic A.B.')
+
+*/
+def call(Map params = [:]) {
+  def excludeFlag = params.containsKey('exclude') ? "-exclude ${params.exclude}" : ''
+  def fileExtFlag = params.containsKey('ext') ? "-ext ${params.ext}" : ''
+  def licenseFlag = params.containsKey('license') ? "-license ${params.license}" : ''
+  def licensorFlag = params.containsKey('licensor') ? "-licensor \"${params.licensor}\"" : ''
+  def skipFlag = params.get('skip', false) ? '-d' : ''
+
+  docker.image('golang:1.12').inside("-e HOME=${env.WORKSPACE}/${env.BASE_DIR ?: ''}"){
+    sh(label: 'Check Licenses', script: """
+    go get -u github.com/elastic/go-licenser
+    go-licenser ${skipFlag} ${excludeFlag} ${fileExtFlag} ${licenseFlag} ${licensorFlag}""")
+  }
+}
