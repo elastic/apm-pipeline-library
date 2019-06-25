52ee59b8ba342dc47aad18f133dcd9f726c881f2
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-06-25 17:08:33 +0100

    ci: refactor ITs maps to be centralised and being reused if required

---
+++ src/test/groovy/ItsStepTests.groovy
@@ -0,0 +1,106 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+import com.lesfurets.jenkins.unit.BasePipelineTest
+import org.junit.Before
+import org.junit.Test
+import static com.lesfurets.jenkins.unit.MethodCall.callArgsToString
+import static org.junit.Assert.assertTrue
+
+class ItsStepTests extends BasePipelineTest {
+  String scriptName = "vars/its.groovy"
+  Map env = [:]
+
+  @Override
+  @Before
+  void setUp() throws Exception {
+    super.setUp()
+    binding.setVariable('env', env)
+    helper.registerAllowedMethod("error", [String.class], { s ->
+      updateBuildStatus('FAILURE')
+      throw new Exception(s)
+    })
+  }
+
+  @Test
+  void testEmptyArgumentInAgentYamlVar() throws Exception {
+    def script = loadScript(scriptName)
+    try {
+      script.agentYamlVar('')
+    } catch(e){
+      //NOOP
+    }
+    printCallStack()
+    assertTrue(helper.callStack.findAll { call ->
+      call.methodName == 'error'
+    }.any { call ->
+      callArgsToString(call).contains('agentYamlVar: Missing key')
+    })
+    assertJobStatusFailure()
+  }
+
+  @Test
+  void testEmptyArgumentInMapAgentsIDs() throws Exception {
+    def script = loadScript(scriptName)
+    try {
+      script.mapAgentsIDs('')
+    } catch(e){
+      //NOOP
+    }
+    printCallStack()
+    assertTrue(helper.callStack.findAll { call ->
+      call.methodName == 'error'
+    }.any { call ->
+      callArgsToString(call).contains('mapAgentsIDs: Missing key')
+    })
+    assertJobStatusFailure()
+  }
+
+  @Test
+  void testNullArgumentInMapAgentsApps() throws Exception {
+    def script = loadScript(scriptName)
+    try {
+      script.mapAgentsApps(null)
+    } catch(e){
+      //NOOP
+    }
+    printCallStack()
+    assertTrue(helper.callStack.findAll { call ->
+      call.methodName == 'error'
+    }.any { call ->
+      callArgsToString(call).contains('mapAgentsApps: Missing key')
+    })
+    assertJobStatusFailure()
+  }
+
+  @Test
+  void testNullArgumentInYmlFiles() throws Exception {
+    def script = loadScript(scriptName)
+    try {
+      script.ymlFiles(null)
+    } catch(e){
+      //NOOP
+    }
+    printCallStack()
+    assertTrue(helper.callStack.findAll { call ->
+      call.methodName == 'error'
+    }.any { call ->
+      callArgsToString(call).contains('ymlFiles: Missing key')
+    })
+    assertJobStatusFailure()
+  }
+}
---
+++ vars/its.groovy
@@ -0,0 +1,114 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+/**
+  Return the value for the given key.
+
+  its.agentYamlVar(key)
+  its.mapAgentsApps(key)
+  its.mapAgentsIDs(key)
+  its.ymlFiles(key)
+*/
+import groovy.transform.Field
+
+/**
+  Key which contains the agent versions.
+*/
+@Field Map agentYamlVar = [
+  'dotnet': 'DOTNET_AGENT',
+  'go': 'GO_AGENT',
+  'java': 'JAVA_AGENT',
+  'nodejs': 'NODEJS_AGENT',
+  'python': 'PYTHON_AGENT',
+  'ruby': 'RUBY_AGENT',
+  'rum': 'RUM_AGENT',
+  'server': 'APM_SERVER'
+]
+
+/**
+  translate from human agent name to its app name .
+*/
+@Field Map mapAgentsApps = [
+  '.NET': 'dotnet',
+  'Go': 'go-net-http',
+  'Java': 'java-spring',
+  'Node.js': 'nodejs-express',
+  'Python': 'python-django',
+  'Ruby': 'ruby-rails',
+  'RUM': 'rumjs',
+  'All': 'all',
+  'UI': 'ui'
+]
+
+/**
+  translate from human agent name to an ID.
+*/
+@Field Map mapAgentsIDs = [
+  '.NET': 'dotnet',
+  'Go': 'go',
+  'Java': 'java',
+  'Node.js': 'nodejs',
+  'Python': 'python',
+  'Ruby': 'ruby',
+  'RUM': 'rum',
+  'All': 'all',
+  'UI': 'ui'
+]
+
+/**
+  YAML files to get agent versions and exclusions.
+*/
+@Field Map ymlFiles = [
+  'dotnet': 'tests/versions/dotnet.yml',
+  'go': 'tests/versions/go.yml',
+  'java': 'tests/versions/java.yml',
+  'nodejs': 'tests/versions/nodejs.yml',
+  'python': 'tests/versions/python.yml',
+  'ruby': 'tests/versions/ruby.yml',
+  'rum': 'tests/versions/rum.yml',
+  'server': 'tests/versions/apm_server.yml'
+]
+
+def agentYamlVar(String key) {
+  if (!key) {
+    error 'agentYamlVar: Missing key'
+  }
+
+  return agentYamlVar[key]
+}
+
+def mapAgentsIDs(String key) {
+  if (!key) {
+    error 'mapAgentsIDs: Missing key'
+  }
+
+  return mapAgentsIDs[key]
+}
+
+def mapAgentsApps(String key) {
+  if (!key) {
+    error 'mapAgentsApps: Missing key'
+  }
+  return mapAgentsApps[key]
+}
+
+def ymlFiles(String key) {
+  if (!key) {
+    error 'ymlFiles: Missing key'
+  }
+  return ymlFiles[key]
+}
