c99ebafcb70d27eeef5610865141f2b9aadac427
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2019-07-01 18:25:29 +0200

    feat: generate release notes and changelog from PRs

--- resources/scripts/jenkins/release-notes.sh
+++ resources/scripts/jenkins/release-notes.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -uxeo pipefail
 
-gren release --username=elastic --override -c .grenrc.js -t all
+gren release --override -c .grenrc.js -t all
 # it is generated from scratch to have reverse version order
-gren changelog --username=elastic --override -c .grenrc.js -t all -G
+gren changelog --override -c .grenrc.js -t all -G
