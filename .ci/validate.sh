e907f0e57f00533bc8ce9c9cbecb621e5d0b59f8
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-07-09 22:00:49 +0100

    support crumb

--- .ci/validate.sh
+++ .ci/validate.sh
@@ -3,10 +3,13 @@ set +e
 
 if [ -z ${JENKINS_URL} ] ; then
   JENKINS_URL=http://0.0.0.0:18080
+else
+  CRUMB=$(curl --silent ${JENKINS_URL}/crumbIssuer/api/xml?xpath=concat\(//crumbRequestField,%22:%22,//crumb\))
+  FLAG="-H \"${CRUMB}\""
 fi
 
 for file in "$@"; do
-  curl --silent -X POST -F "jenkinsfile=<${file}" ${JENKINS_URL}/pipeline-model-converter/validate | grep -i -v successfully
+  curl --silent -X POST ${FLAG} -F "jenkinsfile=<${file}" ${JENKINS_URL}/pipeline-model-converter/validate | grep -i -v successfully
   if [ $? -eq 0 ] ; then
     echo "ERROR: jenkinslint failed for the file '${file}'"
     exit_status=1
