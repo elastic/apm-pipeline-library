c9b4e0794bcb220dfe886f1bea2fd6ba30159e9e
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-08-12 12:55:07 +0100

    Support withVaultToken in the preCommit

--- vars/preCommit.groovy
+++ vars/preCommit.groovy
@@ -45,11 +45,12 @@ def call(Map params = [:]) {
     if (registry && secretRegistry) {
       dockerLogin(secret: "${secretRegistry}", registry: "${registry}")
     }
-
-    sh """
-      curl https://pre-commit.com/install-local.py | python -
-      git diff-tree --no-commit-id --name-only -r ${commit} | xargs pre-commit run --files | tee ${reportFileName}
-    """
+    withVaultToken {
+      sh """
+        curl https://pre-commit.com/install-local.py | python -
+        git diff-tree --no-commit-id --name-only -r ${commit} | xargs pre-commit run --files | tee ${reportFileName}
+      """
+    }
   }
   if(junitFlag) {
     preCommitToJunit(input: reportFileName, output: "${reportFileName}.xml")
