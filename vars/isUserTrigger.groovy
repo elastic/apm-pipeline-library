e704ed7b4e7e5c67a0722a093cee2f201f359b7d
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-09-02 09:07:24 +0100

    fix: net.sf.json.JSONNull when authentication is disabled

--- vars/isUserTrigger.groovy
+++ vars/isUserTrigger.groovy
@@ -23,10 +23,14 @@
 def call(){
   def buildCause = currentBuild.getBuildCauses()?.find{ it._class == 'hudson.model.Cause$UserIdCause'}
   def found = false
-  log(level: 'DEBUG', text: "isUserTrigger: ${buildCause?.userId}")
-  if(buildCause?.userId?.trim()){
-    env.BUILD_CAUSE_USER = buildCause?.userId
-    found = true
+  log(level: 'DEBUG', text: "isUserTrigger: ${buildCause?.userId?.toString()}")
+  if (buildCause) {
+    if (buildCause instanceof net.sf.json.JSONNull || buildCause.userId instanceof net.sf.json.JSONNull) {
+      found = false
+    } else if (buildCause?.userId?.trim()) {
+      env.BUILD_CAUSE_USER = buildCause?.userId
+      found = true
+    }
   }
   return found
 }
