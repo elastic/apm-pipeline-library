ca79554f9e19375688307bdfced936ce3ba2e943
Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>
2019-09-11 09:43:08 +0200

    ci(jenkins): update opbeans Docker images build job (#224)

--- .ci/packer_cache.sh
+++ .ci/packer_cache.sh
@@ -3,23 +3,24 @@
 # shellcheck disable=SC1091
 source /usr/local/bin/bash_standard_lib.sh
 
-# docker.elastic.co/observability-ci/apm-integration-testing-tests-rum
-# docker.elastic.co/observability-ci/apm-integration-testing-tests-ruby-rails
-# docker.elastic.co/observability-ci/apm-integration-testing-tests-python-flask
-# docker.elastic.co/observability-ci/apm-integration-testing-tests-python-django
-# docker.elastic.co/observability-ci/apm-integration-testing-tests-nodejs-express
-# docker.elastic.co/observability-ci/apm-integration-testing-tests-java-spring
-# docker.elastic.co/observability-ci/apm-integration-testing-tests-go-nethttp
-# docker.elastic.co/observability-ci/apm-integration-testing-tests-dotnet
-# docker.elastic.co/observability-ci/apm-integration-testing-tests-opbeans-rum
-# docker.elastic.co/observability-ci/apm-integration-testing-tests-opbeans-ruby
-# docker.elastic.co/observability-ci/apm-integration-testing-tests-opbeans-python
-# docker.elastic.co/observability-ci/apm-integration-testing-tests-opbeans-node
-# docker.elastic.co/observability-ci/apm-integration-testing-tests-opbeans-java
-# docker.elastic.co/observability-ci/apm-integration-testing-tests-opbeans-go
-# docker.elastic.co/observability-ci/apm-integration-testing-tests-opbeans-frontend
-# docker.elastic.co/observability-ci/apm-integration-testing-tests-opbeans-dotnet
-# docker.elastic.co/observability-ci/apm-integration-testing-tests-apm-server
+# docker.elastic.co/observability-ci/it_rum
+# docker.elastic.co/observability-ci/it_ruby-rails
+# docker.elastic.co/observability-ci/it_python-flask
+# docker.elastic.co/observability-ci/it_python-django
+# docker.elastic.co/observability-ci/it_nodejs-express
+# docker.elastic.co/observability-ci/it_java-spring
+# docker.elastic.co/observability-ci/it_go-nethttp
+# docker.elastic.co/observability-ci/it_dotnet
+# docker.elastic.co/observability-ci/it_opbeans-rum
+# docker.elastic.co/observability-ci/it_opbeans-ruby
+# docker.elastic.co/observability-ci/it_opbeans-python
+# docker.elastic.co/observability-ci/it_opbeans-node
+# docker.elastic.co/observability-ci/it_opbeans-java
+# docker.elastic.co/observability-ci/it_opbeans-go
+# docker.elastic.co/observability-ci/it_opbeans-frontend
+# docker.elastic.co/observability-ci/it_opbeans-frontend_nginx
+# docker.elastic.co/observability-ci/it_opbeans-dotnet
+# docker.elastic.co/observability-ci/it_apm-server
 
 DOCKER_IMAGES="alpine:3.4
 alpine:3.10.1
