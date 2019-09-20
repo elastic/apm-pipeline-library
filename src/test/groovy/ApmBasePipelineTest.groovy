5efc5bc6a2b252d857cc145f1d81594a3b14a223
Victor Martinez <victormartinezrubio@gmail.com>
2019-09-20 13:39:20 +0100

    enable opbeans pipeline (#230)

---
+++ src/test/groovy/ApmBasePipelineTest.groovy
@@ -0,0 +1,119 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+import com.lesfurets.jenkins.unit.BasePipelineTest
+
+class ApmBasePipelineTest extends BasePipelineTest {
+  Map env = [:]
+
+  @Override
+  void setUp() {
+    super.setUp()
+    env.BRANCH_NAME = 'master'
+    binding.setVariable('env', env)
+
+    registerDeclarativeMethods()
+    registerScriptedMethods()
+    registerSharedLibraryMethods()
+  }
+
+  void registerDeclarativeMethods() {
+    helper.registerAllowedMethod('always', [Closure.class], null)
+    helper.registerAllowedMethod('ansiColor', [String.class], null)
+    helper.registerAllowedMethod('agent', [Closure.class], null)
+    helper.registerAllowedMethod('beforeAgent', [Boolean.class], { true })
+    helper.registerAllowedMethod('disableResume', [], null)
+    helper.registerAllowedMethod('durabilityHint', [String.class], null)
+    helper.registerAllowedMethod('issueCommentTrigger', [String.class], null)
+    helper.registerAllowedMethod('label', [String.class], null)
+    helper.registerAllowedMethod('options', [Closure.class], null)
+    helper.registerAllowedMethod('pipeline', [Closure.class], null)
+    helper.registerAllowedMethod('post', [Closure.class], null)
+    helper.registerAllowedMethod('quietPeriod', [Integer.class], null)
+    helper.registerAllowedMethod('rateLimitBuilds', [Map.class], null)
+    helper.registerAllowedMethod('stage', [Closure.class], null)
+    helper.registerAllowedMethod('stage', [String.class, Closure.class], { stageName, body ->
+      def stageResult
+      helper.registerAllowedMethod('when', [Closure.class], { bodyWhen ->
+        helper.registerAllowedMethod('branch', [String.class], { branchName  ->
+          if(branchName == env.BRANCH_NAME) {
+            return true
+          }
+          throw new RuntimeException("Stage \"${stageName}\" skipped due to when conditional")
+        })
+        return bodyWhen()
+      })
+
+      switch (currentBuild.result) {
+        case 'FAILURE':
+          break
+        default:
+          try {
+            stageResult = body()
+          }
+          catch (RuntimeException re) {
+            // skip stage due to when conditional
+          }
+          catch (Exception e) {
+            throw e
+          }
+      }
+      return stageResult
+    })
+    helper.registerAllowedMethod('stages', [Closure.class], null)
+    helper.registerAllowedMethod('stash', [Map.class], null)
+    helper.registerAllowedMethod('steps', [Closure.class], null)
+    helper.registerAllowedMethod('timeout', [Map.class], null)
+    helper.registerAllowedMethod('timestamps', [], null)
+    helper.registerAllowedMethod('triggers', [Closure.class], null)
+  }
+
+  void registerScriptedMethods() {
+    helper.registerAllowedMethod('credentials', [String.class], { s -> s })
+    helper.registerAllowedMethod('deleteDir', [], null)
+    helper.registerAllowedMethod('dir', [String.class], null)
+    helper.registerAllowedMethod('environment', [Closure.class], { Closure c ->
+
+        def envBefore = [env: binding.getVariable('env')]
+        println "Env section - original env vars: ${envBefore.toString()}"
+        c.resolveStrategy = Closure.DELEGATE_FIRST
+        c.delegate = envBefore
+        c()
+
+        def envNew = envBefore.env
+        envBefore.each { k, v ->
+          if (k != 'env') {
+              envNew["$k"] = v
+          }
+
+        }
+        println "Env section - env vars set to: ${envNew.toString()}"
+        binding.setVariable('env', envNew)
+    })
+    helper.registerAllowedMethod('junit', [Map.class], null)
+    helper.registerAllowedMethod('sh', [String.class], null)
+    helper.registerAllowedMethod('timeout', [Integer.class, Closure.class], null)
+    helper.registerAllowedMethod('unstash', [String.class], null)
+  }
+
+  void registerSharedLibraryMethods() {
+    helper.registerAllowedMethod('dockerLogin', [Map.class], { true })
+    helper.registerAllowedMethod('gitCheckout', [Map.class], null)
+    helper.registerAllowedMethod('notifyBuildResult', [], null)
+    helper.registerAllowedMethod('withGithubNotify', [Map.class, Closure.class], null)
+  }
+}
---
+++ vars/opbeansPipeline.groovy
@@ -0,0 +1,117 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+/**
+  Opbeans Pipeline
+*/
+
+def call(Map pipelineParams) {
+  pipeline {
+    agent { label 'linux && immutable' }
+    environment {
+      BASE_DIR = 'src/github.com/elastic'
+      NOTIFY_TO = credentials('notify-to')
+      JOB_GCS_BUCKET = credentials('gcs-bucket')
+      JOB_GCS_CREDENTIALS = 'apm-ci-gcs-plugin'
+      DOCKERHUB_SECRET = 'secret/apm-team/ci/elastic-observability-dockerhub'
+      PIPELINE_LOG_LEVEL = 'INFO'
+      PATH = "${env.PATH}:${env.WORKSPACE}/bin"
+      HOME = "${env.WORKSPACE}"
+    }
+    options {
+      timeout(time: 1, unit: 'HOURS')
+      buildDiscarder(logRotator(numToKeepStr: '20', artifactNumToKeepStr: '20', daysToKeepStr: '30'))
+      timestamps()
+      ansiColor('xterm')
+      disableResume()
+      durabilityHint('PERFORMANCE_OPTIMIZED')
+      rateLimitBuilds(throttle: [count: 60, durationName: 'hour', userBoost: true])
+      quietPeriod(10)
+    }
+    triggers {
+      issueCommentTrigger('(?i).*(?:jenkins\\W+)?run\\W+(?:the\\W+)?tests(?:\\W+please)?.*')
+    }
+    stages {
+      /**
+       Checkout the code and stash it, to use it on other stages.
+      */
+      stage('Checkout') {
+        steps {
+          deleteDir()
+          gitCheckout(basedir: BASE_DIR)
+          stash allowEmpty: true, name: 'source', useDefaultExcludes: false
+        }
+      }
+      /**
+      Build the project from code..
+      */
+      stage('Build') {
+        steps {
+          withGithubNotify(context: 'Build') {
+            deleteDir()
+            unstash 'source'
+            dir(BASE_DIR){
+              sh 'make build'
+            }
+          }
+        }
+      }
+      /**
+      Execute unit tests.
+      */
+      stage('Test') {
+        steps {
+          withGithubNotify(context: 'Test', tab: 'tests') {
+            deleteDir()
+            unstash 'source'
+            dir(BASE_DIR){
+              sh "make test"
+            }
+          }
+        }
+        post {
+          always {
+            junit(allowEmptyResults: true,
+              keepLongStdio: true,
+              testResults: "${BASE_DIR}/**/junit-*.xml")
+          }
+        }
+      }
+      stage('Release') {
+        when {
+          branch 'master'
+          beforeAgent true
+        }
+        steps {
+          withGithubNotify(context: 'Release') {
+            deleteDir()
+            unstash 'source'
+            dir(BASE_DIR){
+              dockerLogin(secret: "${DOCKERHUB_SECRET}", registry: 'docker.io')
+              sh "VERSION=latest make publish"
+            }
+          }
+        }
+      }
+    }
+    post {
+      always {
+        notifyBuildResult()
+      }
+    }
+  }
+}
