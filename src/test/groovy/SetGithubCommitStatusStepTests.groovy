e58b44badd8cbff79272a110e5ebf9419c7afde7
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-09-20 15:27:45 +0100

    refactor SetGithubCommitStatusStepTests

--- src/test/groovy/SetGithubCommitStatusStepTests.groovy
+++ src/test/groovy/SetGithubCommitStatusStepTests.groovy
@@ -15,29 +15,23 @@
 // specific language governing permissions and limitations
 // under the License.
 
-import com.lesfurets.jenkins.unit.BasePipelineTest
 import org.junit.Before
 import org.junit.Test
 import static com.lesfurets.jenkins.unit.MethodCall.callArgsToString
 import static org.junit.Assert.assertTrue
 
-class SetGithubCommitStatusStepTests extends BasePipelineTest {
-
-  String url = 'http://github.com/org/repo.git'
-  String sha = '29480a51'
+class SetGithubCommitStatusStepTests extends ApmBasePipelineTest {
+  String scriptName = 'vars/setGithubCommitStatus.groovy'
 
   @Override
   @Before
   void setUp() throws Exception {
     super.setUp()
-
-    helper.registerAllowedMethod('getGitRepoURL', [], {return url})
-    helper.registerAllowedMethod('getGitCommitSha', [], {return sha})
   }
 
   @Test
   void test() throws Exception {
-    def script = loadScript("vars/setGithubCommitStatus.groovy")
+    def script = loadScript(scriptName)
     script.call()
     printCallStack()
     assertJobStatusSuccess()
@@ -45,8 +39,8 @@ class SetGithubCommitStatusStepTests extends BasePipelineTest {
 
   @Test
   void testParams() throws Exception {
-    def script = loadScript("vars/setGithubCommitStatus.groovy")
-    script.call(repoUrl: url, commitSha: sha, message: 'Build result.', state: "SUCCESS")
+    def script = loadScript(scriptName)
+    script.call(repoUrl: REPO_URL, commitSha: SHA, message: 'Build result.', state: "SUCCESS")
     printCallStack()
     assertJobStatusSuccess()
   }
