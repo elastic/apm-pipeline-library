5b2ba5ac26434d71713cab0ce887472475cf97e5
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-09-26 11:36:56 +0100

    to support current pipelines

--- local/workers/windows/swarm.bat
+++ local/workers/windows/swarm.bat
@@ -1,1 +1,1 @@
-java -jar c:\\Users\\vagrant\\swarm-client.jar -labels "windows %COMPUTERNAME%" -master http://10.0.2.2:18080 -fsroot c:\\jenkins -deleteExistingClients
+java -jar c:\\Users\\vagrant\\swarm-client.jar -labels "windows %COMPUTERNAME% %COMPUTERNAME%-immutable" -master http://10.0.2.2:18080 -fsroot c:\\jenkins -deleteExistingClients
