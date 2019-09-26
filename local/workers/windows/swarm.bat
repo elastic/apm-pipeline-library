d00a59945740612f73d015fb78a46ba5a34c1c3e
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-09-26 11:36:21 +0100

    ensure the fsroot is somewhere else, and use the hostname as label

--- local/workers/windows/swarm.bat
+++ local/workers/windows/swarm.bat
@@ -1,1 +1,1 @@
-java -jar c:\\Users\\vagrant\\swarm-client.jar -labels "windows" -master http://10.0.2.2:18080
+java -jar c:\\Users\\vagrant\\swarm-client.jar -labels "windows %COMPUTERNAME%" -master http://10.0.2.2:18080 -fsroot c:\\jenkins -deleteExistingClients
