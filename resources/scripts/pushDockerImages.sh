f54983249030e6817cf6f866b1ce3b0b6604711f
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-11-15 15:13:55 +0000

    first iteration: push images if they have not been pushed yet

--- resources/scripts/pushDockerImages.sh
+++ resources/scripts/pushDockerImages.sh
@@ -32,11 +32,34 @@ PREFIX=${2:-}
 PUSH_VERSION=${3:-"daily"}
 REGISTRY=${4:-"docker.elastic.co"}
 
+## Prepare the context
+if [ -e images.sha256.txt ] ; then
+  rm images.sha256.txt
+fi
+
 VERSION=$(curl -s "https://artifacts-api.elastic.co/v1/versions/${VERSION}/builds/latest"|jq -r .build.version)
 
 for image in elasticsearch/elasticsearch kibana/kibana apm/apm-server beats/auditbeat beats/filebeat beats/heartbeat beats/metricbeat beats/packetbeat
 do
+  echo "Let's cache the docker image for ${image}"
   IMAGE_SHORT=${image#*/}
-  docker tag "${REGISTRY}/${image}:${VERSION}" "${REGISTRY}/${PREFIX}/${IMAGE_SHORT}:${PUSH_VERSION}"
-  docker push "${REGISTRY}/${PREFIX}/${IMAGE_SHORT}:${PUSH_VERSION}"
+  IMAGE_ALREADY_CACHE="${REGISTRY}/${image}:${VERSION}"
+  IMAGE_TO_BE_PUSHED_IF_CHANGED="${REGISTRY}/${PREFIX}/${IMAGE_SHORT}:${PUSH_VERSION}"
+
+  ## Get sha256
+  SHA_IMAGE_ALREADY_CACHE=$(docker inspect --format='{{index .RepoDigests 0}}' "${IMAGE_ALREADY_CACHE}")
+  docker pull "${IMAGE_TO_BE_PUSHED_IF_CHANGED}" > /dev/null
+  SHA_IMAGE_TO_BE_PUSHED_IF_CHANGED=$(docker inspect --format='{{index .RepoDigests 0}}' "${IMAGE_TO_BE_PUSHED_IF_CHANGED}")
+  docker rmi -f "${IMAGE_TO_BE_PUSHED_IF_CHANGED}" > /dev/null
+
+  ## It has not been cached then
+  if [ "$SHA_IMAGE_ALREADY_CACHE" != "$SHA_IMAGE_TO_BE_PUSHED_IF_CHANGED" ]; then
+    docker tag "${IMAGE_ALREADY_CACHE}" "${IMAGE_TO_BE_PUSHED_IF_CHANGED}"
+    docker push "${IMAGE_TO_BE_PUSHED_IF_CHANGED}"
+  else
+    echo "Image '${image}' was already pushed"
+  fi
+
+  ## Create file
+  echo "${IMAGE_TO_BE_PUSHED_IF_CHANGED}:${SHA_IMAGE_TO_BE_PUSHED_IF_CHANGED}" >> images.sha256.txt
 done
