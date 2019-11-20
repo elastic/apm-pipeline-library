6a6536a7ba158ddc646f9feb895e2492275d53b0
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-11-20 15:29:22 +0000

    force the jenkins in the comment trigger

--- vars/opbeansPipeline.groovy
+++ vars/opbeansPipeline.groovy
@@ -52,7 +52,7 @@ def call(Map pipelineParams) {
       quietPeriod(10)
     }
     triggers {
-      issueCommentTrigger('(?i).*(?:jenkins\\W+)?run\\W+(?:the\\W+)?tests(?:\\W+please)?.*')
+      issueCommentTrigger('(?i).*jenkins\\W+run\\W+(?:the\\W+)?tests(?:\\W+please)?.*')
     }
     stages {
       /**
