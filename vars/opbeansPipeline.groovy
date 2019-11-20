01a1d98fd31da2ecbf0fc3acdfe1805d521c70b8
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-11-20 16:09:33 +0000

    chore: for testing purposes

--- vars/opbeansPipeline.groovy
+++ vars/opbeansPipeline.groovy
@@ -141,6 +141,7 @@ def call(Map pipelineParams) {
           anyOf {
             branch 'master'
             tag pattern: 'v\\d+\\.\\d+.*', comparator: 'REGEXP'
+            branch 'PR-29'
           }
           beforeAgent true
         }
