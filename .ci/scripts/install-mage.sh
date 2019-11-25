6ef14ab527b635ba95393fabf7ee6918c4f0effc
Manuel de la Peña <manuel.delapena@elastic.co>
2019-11-25 12:11:05 +0100

    (#213) Install Go in the build agent before installing mage

--- .ci/scripts/install-mage.sh
+++ .ci/scripts/install-mage.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
-set -eo pipefail
+set -euxo pipefail
+
+#
+# Install the given go version using the gimme script.
+#
+# Parameters:
+#   - GO_VERSION - that's the version which will be installed and enabled. v1.12.7 by default
+#
+
+GO_VERSION="${1:-1.12.7}"
+
+# Install Go using the same travis approach
+echo "Installing ${GO_VERSION} with gimme."
+eval "$(curl -sL https://raw.githubusercontent.com/travis-ci/gimme/master/gimme | GIMME_GO_VERSION=${GO_VERSION} bash)"
 
 # From https://magefile.org/#installation
 go get -u -d github.com/magefile/mage
