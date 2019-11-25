a6be5e7670c9733030ca91fc7a91367f6d1584c6
Victor Martinez <victormartinezrubio@gmail.com>
2019-11-25 13:17:54 +0000

    [pre-commit] Default apm-ci jenkins instance and validate ./Jenkinsfile (#281)

--- .ci/scripts/validate.sh
+++ .ci/scripts/validate.sh
@@ -2,7 +2,7 @@
 set -eo pipefail
 
 if [ -z "${JENKINS_URL}" ] ; then
-  JENKINS_URL=http://0.0.0.0:18080
+  JENKINS_URL=https://apm-ci.elastic.co
 else
   # See https://jenkins.io/doc/book/pipeline/development/#linter
   JENKINS_URL=${JENKINS_URL%%/}  ## Remove last / if it's defined
