fca7e13aad04c80cb7093051554fe6747ae1e46b
Manuel de la Peña <manuel.delapena@elastic.co>
2019-11-25 16:34:50 +0100

    (#213) Install Go in the build agent before installing mage (#280)
    * (#213) Install Go in the build agent before installing mage
    
    * (#213) Support installing Go from a known version
    
    We'll use a file at workspace, but it could be overriden by an environment variable,
    that will allow using different Go versions

--- .ci/scripts/install-mage.sh
+++ .ci/scripts/install-mage.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/env bash
-set -eo pipefail
+set -euxo pipefail
+
+#
+# Install the given go version using the gimme script.
+#
+# Parameters:
+#   - GO_VERSION - that's the version which will be installed and enabled. v1.12.7 by default
+#
+
+GO_VERSION="1.12.7"
+
+if [ -f ".go_version" ]; then
+  GO_VERSION=$(cat .go_version)
+fi
+
+if [ -n "$1" ]; then
+  GO_VERSION="${1}"
+fi
+
+# Install Go using the same travis approach
+echo "Installing ${GO_VERSION} with gimme."
+eval "$(curl -sL https://raw.githubusercontent.com/travis-ci/gimme/master/gimme | GIMME_GO_VERSION=${GO_VERSION} bash)"
 
 # From https://magefile.org/#installation
 go get -u -d github.com/magefile/mage
