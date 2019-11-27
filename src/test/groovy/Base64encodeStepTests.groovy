c9393ba3082221ba1f9c25acc0d3487a321e7333
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-11-27 12:10:38 +0000

    Unrequired import

--- src/test/groovy/Base64encodeStepTests.groovy
+++ src/test/groovy/Base64encodeStepTests.groovy
@@ -18,7 +18,6 @@
 import org.junit.Before
 import org.junit.Test
 import java.util.Base64
-import static com.lesfurets.jenkins.unit.MethodCall.callArgsToString
 import static org.junit.Assert.assertTrue
 
 class Base64encodeStepTests extends ApmBasePipelineTest {
