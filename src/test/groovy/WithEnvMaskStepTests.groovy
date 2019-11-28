0868e66ac56829b9724dc73aa678d549c8ef4f81
Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>
2019-11-28 18:02:00 +0100

    feat: setEnvVar, whenTrue, whenFalse, and withEnvMask new steps (#295)
    * feat: setEnvVar, whenTrue, whenFalse, and withEnvMask new steps
    
    * feat: it is better if I push the code :D
    
    * Update vars/withEnvMask.groovy
    
    Co-Authored-By: Victor Martinez <victormartinezrubio@gmail.com>

---
+++ src/test/groovy/WithEnvMaskStepTests.groovy
@@ -0,0 +1,77 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+import org.junit.Before
+import org.junit.Test
+import static org.junit.Assert.assertTrue
+
+class WithEnvMaskStepTests extends ApmBasePipelineTest {
+  String scriptName = 'vars/withEnvMask.groovy'
+
+  @Override
+  @Before
+  void setUp() throws Exception {
+    super.setUp()
+  }
+
+  @Test
+  void test() throws Exception {
+    def script = loadScript(scriptName)
+    def isOK = false
+    script.call(vars: [
+      [var: "CYPRESS_user", password: "user"],
+      [var: "CYPRESS_password", password: "password"]
+    ]){
+      isOK = true
+    }
+    printCallStack()
+    assertTrue(isOK)
+    assertJobStatusSuccess()
+  }
+
+  @Test
+  void testNull() throws Exception {
+    def script = loadScript(scriptName)
+    def isOK = false
+    script.call(vars: null){
+      isOK = true
+    }
+    printCallStack()
+    assertTrue(isOK)
+    assertJobStatusSuccess()
+  }
+
+  @Test
+  void testParams() throws Exception {
+    def script = loadScript(scriptName)
+    def isOK = false
+    script.call(vars: [
+      [var: "CYPRESS_user", password: "user"],
+      [var: "CYPRESS_password", password: "password"]
+    ]){
+      if(binding.getVariable("CYPRESS_user") == "user"
+      && binding.getVariable("CYPRESS_password") == "password"){
+        isOK = true
+      }
+    }
+    printCallStack()
+    assertTrue(isOK)
+    assertTrue(assertMethodCallContainsPattern('wrap', 'class=MaskPasswordsBuildWrapper, varPasswordPairs=[{var=CYPRESS_user, password=user}, {var=CYPRESS_password, password=password}]'))
+    assertTrue(assertMethodCallContainsPattern('withEnv', '[CYPRESS_user=user, CYPRESS_password=password]'))
+    assertJobStatusSuccess()
+  }
+}
