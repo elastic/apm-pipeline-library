9736c198af1ad9622ce048e517a8d5ff0c3b25f0
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2020-01-14 18:00:26 +0100

    fix: add ubuntu label and windows-immutable label

--- local/workers/windows/swarm.bat
+++ local/workers/windows/swarm.bat
@@ -1,3 +1,3 @@
 set "str=%COMPUTERNAME%"
 for /f "usebackq delims=" %%I in (`powershell "\"%str%\".toLower()"`) do set "lower=%%~I"
-java -jar c:\\Users\\vagrant\\swarm-client.jar -labels "windows %lower% %lower%-immutable" -master http://10.0.2.2:18080 -fsroot c:\\jenkins -deleteExistingClients
+java -jar c:\\Users\\vagrant\\swarm-client.jar -labels "windows %lower% %lower%-immutable windows-immutable" -master http://10.0.2.2:18080 -fsroot c:\\jenkins -deleteExistingClients
