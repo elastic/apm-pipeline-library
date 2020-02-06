6d1677bb50e526c53ca6c40068be4805b1257751
Mike Place <mike.place@elastic.co>
2020-02-06 13:32:55 +0100

    Start on nexus test framework

---
+++ src/test/groovy/NexusTests.groovy
@@ -0,0 +1,41 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+import org.junit.Before
+import org.junit.Test
+import static org.junit.Assert.assertTrue
+
+class NexusTests extends ApmBasePipelineTest {
+  String scriptName = 'src/co/elatsic/Nexus.groovy'
+
+  @Override
+  @Before
+  void setUp() throws Exception {
+    super.setUp()
+  }
+
+  @Test
+  void test() throws Exception {
+    def script = loadScript(scriptName)
+    script.createConnection(
+      "http://localhost:9999",
+      "dummy_user",
+      "dummy_pass",
+      "/dummy_path"
+      )
+  }
+}
