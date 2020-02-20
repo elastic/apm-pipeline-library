cbf7ae0fda61b6d8af64cc9425f0201fdef3c2dd
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-02-20 10:58:29 +0000

    ci(jenkins): dummy declarative pipeline with parameters

---
+++ src/test/groovy/dummyDeclarativePipelineStepTests.groovy
@@ -0,0 +1,55 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+import org.junit.Before
+import org.junit.Test
+import static org.junit.Assert.assertTrue
+
+class DummyDeclarativePipelineStepTests extends ApmBasePipelineTest {
+  String scriptName = 'vars/dummyDeclarativePipeline.groovy'
+
+  @Override
+  @Before
+  void setUp() throws Exception {
+    binding.setProperty('BASE_DIR', '/')
+    super.setUp()
+  }
+
+  @Test
+  void test_with_defaults() throws Exception {
+    def script = loadScript(scriptName)
+    params.BUILD_OPTS = ''
+    script.call()
+    printCallStack()
+    assertTrue(assertMethodCallContainsPattern('stage', 'Checkout'))
+    assertTrue(assertMethodCallContainsPattern('stage', 'Build'))
+    assertTrue(assertMethodCallContainsPattern('echo', "FOO=''"))
+    assertJobStatusSuccess()
+  }
+
+  @Test
+  void test_with_foo_params() throws Exception {
+    def script = loadScript(scriptName)
+    params.FOO = 'BAR'
+    script.call()
+    printCallStack()
+    assertTrue(assertMethodCallContainsPattern('stage', 'Checkout'))
+    assertTrue(assertMethodCallContainsPattern('stage', 'Build'))
+    assertTrue(assertMethodCallContainsPattern('echo', "FOO='BAR'"))
+    assertJobStatusSuccess()
+  }
+}
