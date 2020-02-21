11694c3356ac12d336d3a23995f297ad910bc9d5
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-02-21 15:48:35 +0000

    [packer-cache] Cache jjbb images

--- .ci/packer_cache.sh
+++ .ci/packer_cache.sh
@@ -27,11 +27,13 @@ alpine:3.10.1
 node:12-slim
 node:12.7.0-stretch-slim
 python:3.7.4-alpine3.10
+docker.elastic.co/infra/jjbb
 docker.elastic.co/observability-ci/codecov
 docker.elastic.co/observability-ci/golang-mage
 docker.elastic.co/observability-ci/gren
 docker.elastic.co/observability-ci/shellcheck
 docker.elastic.co/observability-ci/yamllint
+widerplan/jenkins-job-builder
 "
 
 for di in ${DOCKER_IMAGES}
