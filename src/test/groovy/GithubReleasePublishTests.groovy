d5adfd86cc43e225516d220588e127ffd7856d67
cachedout <mike.place@elastic.co>
2020-02-25 15:27:36 +0100

    Update src/test/groovy/GithubReleasePublishTests.groovy
    Co-Authored-By: Victor Martinez <victormartinezrubio@gmail.com>

--- src/test/groovy/GithubReleasePublishTests.groovy
+++ src/test/groovy/GithubReleasePublishTests.groovy
@@ -86,7 +86,7 @@ class GithubReleasePublishTests extends ApmBasePipelineTest {
 
   @Test
   void testNoOrg() throws Exception {
-    helper.registerAllowedMethod("githubApiCall", [Map.class], shInterceptor)
+    helper.registerAllowedMethod("githubApiCall", [Map.class], apiInterceptor)
     def script = loadScript(scriptName)
     env.remove("ORG_NAME")  // Will be reset by setUp()
     try {
