8743621f84fc754926e769ffa3f88ea8c4c455d0
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-03-03 11:32:12 +0000

    ci(jenkins): support semantic versioning order

--- resources/scripts/install-with-choco.ps1
+++ resources/scripts/install-with-choco.ps1
@@ -3,7 +3,33 @@ $ErrorActionPreference = "Stop"
 
 Write-Host("Getting latest {0} version for {1} ..." -f $env:TOOL,$env:VERSION)
 & choco list $env:TOOL --exact --by-id-only --all | Select-String -Pattern "$env:TOOL $env:VERSION"
-$Version = $(choco list $env:TOOL --exact --by-id-only --all) | Select-String -Pattern "$env:TOOL $env:VERSION" | %{$_.ToString().split(" ")[1]} | sort | Select-Object -Last 1
+
+# Get the latest version sorted by alphanumerics.
+$DefaultVersion = $(choco list $env:TOOL --exact --by-id-only --all) | Select-String -Pattern "$env:TOOL $env:VERSION" | %{$_.ToString().split(" ")[1]} | sort | Select-Object -Last 1
+
+# Get the latest version sorted by numeric versions, aka support to semantic versioning
+try {
+  $SemVerVersion = $(choco list $env:TOOL --exact --by-id-only --all) | Select-String -Pattern "$env:TOOL $env:VERSION" | %{$_.ToString().split(" ")[1]} | sort {[version] $_} | Select-Object -Last 1
+} catch {
+  ## If the version type accelerator throws an error then let's use the default version
+  $SemVerVersion = $DefaultVersion
+}
+
+# Compare DefaultVersion with SemVerVersion
+try {
+  if ([version]$SemVerVersion -gt [version]$DefaultVersion) {
+    $Version = $SemVerVersion
+  }
+  else {
+    $Version = $DefaultVersion
+  }
+} catch {
+  ## https://invoke-thebrain.com/2018/12/comparing-version-numbers-powershell/
+  ## It might not be possible to compare as the versions might be different, so let's use
+  ## the defaultVersion
+  $Version = $DefaultVersion
+  Write-Host("Comparing version numbers could not be done ... let's use {0} version instead of {1} ..." -f $DefaultVersion,$SemVerVersion)
+}
 
 Write-Host("Installing {0} version: {1} ..." -f $env:TOOL,$Version)
 & choco install $env:TOOL --no-progress -y --version "$Version"
