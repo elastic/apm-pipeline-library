a62ed7ce805619b1efbf8f6be772fbc38c84cd24
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-03-09 12:05:07 +0000

    add description to the assert

--- test-infra/test_windows.py
+++ test-infra/test_windows.py
@@ -2,20 +2,20 @@ import testinfra
 
 def test_docker_installed(host):
   cmd = host.run("docker -v")
-  assert cmd.rc == 0
+  assert cmd.rc == 0, "it is required to be used when running apm-agent-dotnet"
 
 def test_dotnet_installed(host):
   cmd = host.run("dotnet --info")
-  assert cmd.rc == 0
+  assert cmd.rc == 0, "it is required to be used when running apm-agent-dotnet"
 
 def test_msbuild_installed(host):
   cmd = host.run("msbuild")
-  assert cmd.rc == 0
+  assert cmd.rc == 0, "it is required to be used when running apm-agent-dotnet"
 
 def test_nuget_installed(host):
   cmd = host.run("nuget --help")
-  assert cmd.rc == 0
+  assert cmd.rc == 0, "it is required to be used when running apm-agent-dotnet"
 
 def test_python_installed(host):
   cmd = host.run("python --version")
-  assert cmd.rc == 0
+  assert cmd.rc == 0, "it is required to be used when running apm-agent-python"
