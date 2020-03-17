0e5e995cffc1c2bd344003fb6280a2a1121c0488
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-03-17 12:26:40 +0000

    ci(jenkins): release generation without override

--- resources/scripts/jenkins/release-notes.sh
+++ resources/scripts/jenkins/release-notes.sh
@@ -3,6 +3,6 @@ set -uxeo pipefail
 
 GREN_GITHUB_TOKEN=${GREN_GITHUB_TOKEN:?"missing GREN_GITHUB_TOKEN"}
 
-gren release --token="${GREN_GITHUB_TOKEN}" --override -c .grenrc.js -t all
+gren release --token="${GREN_GITHUB_TOKEN}" -c .grenrc.js -t all
 # it is generated from scratch to have reverse version order
 gren changelog --token="${GREN_GITHUB_TOKEN}" --override -c .grenrc.js -t all -G
