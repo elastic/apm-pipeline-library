b9887c245dafb3d439de6fdd7bd8c646f49620f6
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-03-19 10:44:38 +0000

    Skip

--- test-infra/test_installed_tools.py
+++ test-infra/test_installed_tools.py
@@ -1,15 +1,16 @@
 import testinfra
+import pytest
 
 def test_docker_installed(host):
   command = "docker --version"
-  if host.system_info.distribution == 'darwin' :
+  if host.system_info.type == 'darwin' :
     pytest.skip("unsupported configuration")
   else:
     cmd = host.run(command)
   assert cmd.rc == 0, "it is required for all the APM projects"
 
 def test_docker_compose_installed(host):
-  if host.system_info.distribution == 'darwin' :
+  if host.system_info.type == 'darwin' :
     pytest.skip("unsupported configuration")
   else:
     cmd = host.run("docker-compose --version")
@@ -20,7 +21,7 @@ def test_java_installed(host):
   assert cmd.rc == 0, "it is required for the apm-agent-java"
 
 def test_go_installed(host):
-  if host.system_info.distribution == 'darwin' :
+  if host.system_info.type == 'darwin' :
     pytest.skip("unsupported configuration")
   else:
     cmd = host.run("go version")
@@ -43,7 +44,7 @@ def test_python_installed(host):
   assert cmd.rc == 0, "it is required for the apm-agent-python and apm-integration-testing"
 
 def test_python3_installed(host):
-  if host.system_info.distribution == 'darwin' :
+  if host.system_info.type == 'darwin' :
     pytest.skip("unsupported configuration")
   else:
     cmd = host.run("python3 --version")
