7f88a5e7b2879b728b6b74261fdf806ed861f9b2
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-03-19 10:51:15 +0000

    split tests per platform

--- test-infra/test_installed_tools.py
+++ test-infra/test_installed_tools.py
@@ -1,31 +1,9 @@
 import testinfra
 import pytest
 
-def test_docker_installed(host):
-  command = "docker --version"
-  if host.system_info.type == 'darwin' :
-    pytest.skip("unsupported configuration")
-  else:
-    cmd = host.run(command)
-  assert cmd.rc == 0, "it is required for all the APM projects"
-
-def test_docker_compose_installed(host):
-  if host.system_info.type == 'darwin' :
-    pytest.skip("unsupported configuration")
-  else:
-    cmd = host.run("docker-compose --version")
-  assert cmd.rc == 0, "it is required for all the APM projects"
-
 def test_java_installed(host):
   cmd = host.run("java -version")
   assert cmd.rc == 0, "it is required for the apm-agent-java"
-
-def test_go_installed(host):
-  if host.system_info.type == 'darwin' :
-    pytest.skip("unsupported configuration")
-  else:
-    cmd = host.run("go version")
-  assert cmd.rc == 0, "it is required for the apm-agent-go and apm-server"
 
 def test_gvm_installed(host):
   cmd = host.run("gvm --version")
@@ -41,13 +19,6 @@ def test_jq_installed(host):
 
 def test_python_installed(host):
   cmd = host.run("python --version")
-  assert cmd.rc == 0, "it is required for the apm-agent-python and apm-integration-testing"
-
-def test_python3_installed(host):
-  if host.system_info.type == 'darwin' :
-    pytest.skip("unsupported configuration")
-  else:
-    cmd = host.run("python3 --version")
   assert cmd.rc == 0, "it is required for the apm-agent-python and apm-integration-testing"
 
 def test_vault_installed(host):
