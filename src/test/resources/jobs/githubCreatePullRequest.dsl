720d1eca39382a016f50a95f5cde97d2d93e9691
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-04-13 14:55:37 +0100

    ci: enable githubCreatePullRequest

---
+++ src/test/resources/jobs/githubCreatePullRequest.dsl
@@ -0,0 +1,30 @@
+NAME = 'it/githubCreatePullRequest'
+DSL = '''pipeline {
+  agent any
+  stages {
+    stage('checkout') {
+      steps {
+        gitCheckout(credentialsId: '2a9602aa-ab9f-4e52-baf3-b71ca88469c7-UserAndToken',
+                    repo: 'https://github.com/elastic/apm-pipeline-library.git',
+                    branch: 'master',
+                    basedir: 'sub-folder')
+        dir('sub-folder') {
+          setupAPMGitEmail(global: true)
+          sh '''
+            touch githubCreatePullRequest.txt
+            git commit -a -m 'chore: for testing purposes'
+          '''
+          githubCreatePullRequest(title: 'Foo', description: 'Bar', labels: 'invalid', milestone: 'chore', assigne: 'v1v', draft: true)
+        }
+      }
+    }
+  }
+}'''
+
+pipelineJob(NAME) {
+  definition {
+    cps {
+      script(DSL.stripIndent())
+    }
+  }
+}
---
+++ vars/githubCreatePullRequest.groovy
@@ -0,0 +1,43 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+/**
+  Create a Pull Request in GitHub as long as the command runs in the git repo and
+  there are commited changes
+
+  githubCreatePullRequest(title: 'Foo')
+*/
+
+def call(Map params = [:]) {
+  if(!isUnix()) {
+    error 'githubCreatePullRequest: windows is not supported yet.'
+  }
+  def title = params.containsKey('title') ? """--message '${params.title}'""" : error('githubCreatePullRequest: title argument is required.')
+  def description = params.containsKey('description') ? """--message '${params.description}'""" : ''
+  def assign = params.containsKey('assign') ? "--assign ${params.assign}" : ''
+  def reviewer = params.containsKey('reviewer') ? "--reviewer ${params.reviewer}" : ''
+  def milestone = params.containsKey('milestone') ? "--milestone ${params.milestone}" : ''
+  def labels = params.containsKey('labels') ? "--labels ${params.labels}" : ''
+  def draft = params.containsKey('draft') ? params.draft : false
+  def base = params.containsKey('base') ? "--base ${params.base}" : ''
+  def credentialsId = params.get('credentialsId', '2a9602aa-ab9f-4e52-baf3-b71ca88469c7')
+
+  def draftFlag = draft ? '--draft' : ''
+  withCredentials([string(credentialsId: "${credentialsId}", variable: 'GITHUB_TOKEN')]) {
+    sh(label: 'Create GitHub issue', script: "hub pull-request ${title} ${description} ${draftFlag} ${assign} ${reviewer} ${labels} ${milestone} ${base}")
+  }
+}
