3db98afa95cdeafcd40f0b790ed7dd9679b6a816
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-04-23 11:17:46 +0100

    Add verbose output

--- resources/scripts/generate-build-data.sh
+++ resources/scripts/generate-build-data.sh
@@ -21,6 +21,8 @@ BO_BUILD_URL=${2:?'Missing the Blue Ocean Build URL'}
 RESULT=${3:?'Missing the build result'}
 DURATION=${4:?'Missing the build duration'}
 
+echo "INFO: $0 $*"
+
 ## To report the status afterwards
 STATUS=0
 BUILD_INFO="build-info.json"
@@ -59,7 +61,7 @@ function fetch() {
     file=$1
     url=$2
     
-    echo "INFO: generate-build-data - curl ${url} -o ${file}"
+    echo "INFO: curl ${url} -o ${file}"
     ## Let's support retry in the CI.
     if [[ -n "${JENKINS_URL}" && -e "${UTILS_LIB}" ]] ; then
         (retry 3 curlCommand "${file}" "${url}") || STATUS=1
@@ -93,10 +95,13 @@ function fetchAndPrepareBuildInfo() {
         tmp=$(mktemp)
         jq --arg a "${RESULT}" '.result = $a' "${file}" > "$tmp" && mv "$tmp" "${file}"
         jq --arg a "${DURATION}" '.durationInMillis = $a' "${file}" > "$tmp" && mv "$tmp" "${file}"
+        jq '.state = "FINISHED"' "${file}" > "$tmp" && mv "$tmp" "${file}"
     else
         sedCommand "s#\"durationInMillis\":[0-9]*,#\"durationInMillis\":${DURATION},#g" "${file}"
-        sedCommand "s#\"result\":\"[a-zA-Z]*\",#\"result\":\"${RESULT}\",#g" "${file}"
+        sedCommand "s#\"result\":\"[a-zA-Z]*\"#\"result\":\"${RESULT}\"#g" "${file}"
+        sedCommand "s#\"state\":\"[a-zA-Z]*\"#\"state\":\"FINISHED\"#g" "${file}"
     fi
+
     echo "\"${key}\": $(cat "${file}")" >> "${BUILD_REPORT}"
 }
 
