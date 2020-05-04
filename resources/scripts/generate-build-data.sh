e4e8b7a5ea65593878e8f26dfd66472478818c93
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-05-04 13:47:31 +0100

    ci: use folder creation to avoid the file creation corner case

--- resources/scripts/generate-build-data.sh
+++ resources/scripts/generate-build-data.sh
@@ -219,15 +219,17 @@ function fetchAndDefaultStepsInfo() {
         echo "${default}" > "${output}"
     else
          ### Update the displayDescription for those steps with a failure and an empty displayDescription.
-         ###    For instance, when using theh pipeline step `error('foo')`
+         ###    For instance, when using the pipeline step `error('foo')`
          ###    then the 'foo' message is not shown in the BlueOcean restAPI.
          ###
         if jq -e 'map(select(.type=="STEP" and .result=="FAILURE" and .displayDescription==null))' "${output}" > /dev/null ; then
-            tmp=$(mktemp)
+            tmp="$(mktemp -d)/step.log"
             for href in $(jq -r 'map(select(.type=="STEP" and .result=="FAILURE" and .displayDescription==null) | ._links.self.href) | .[]' "${output}"); do
                 id=$(basename "${href}")
                 new=$(curl -s "${BASE_URL}${href}log/" | head -c 100)
                 curlCommand "${tmp}" "${BASE_URL}${href}log/"
+                ## If the URL was unreachable then the file won't exist.
+                ## For such use case, then avoid any transformation.
                 if [ -e "${tmp}" ] ; then
                     new=$(head -c 100 "${tmp}")
                     jq --arg id "${id}" --arg new "${new}" '(.[] | select(.result=="FAILURE" and .displayDescription==null and .id==$id) | .displayDescription) |= $new' "${output}" > "$tmp" && mv "$tmp" "${output}"
