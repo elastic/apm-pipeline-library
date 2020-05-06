c9b1ce9b0f104ebb199dd330c96286f3f8030f3e
Victor Martinez <victormartinezrubio@gmail.com>
2020-05-06 14:20:36 +0100

    ci: precommit installation retry if third party issues (#512)

--- vars/preCommit.groovy
+++ vars/preCommit.groovy
@@ -47,13 +47,22 @@ def call(Map params = [:]) {
       if (registry && secretRegistry) {
         dockerLogin(secret: "${secretRegistry}", registry: "${registry}")
       }
-      sh """
+      retry(2) {
+        sh(label: 'Install precommit', script: """
+          sleep 1
+          curl -s https://pre-commit.com/install-local.py | python -
+        """)
+      }
+      retry(2) {
+        sh(label: 'Install precommit hooks', script: """
+          sleep 1
+          export PATH=${newHome}/bin:${env.PATH}
+          ## Install with the hooks therefore ~/.cache/pre-commit will be created with the repos
+          pre-commit install --install-hooks
+        """)
+      }
+      sh(label: 'Run precommit', script: """
         export PATH=${newHome}/bin:${env.PATH}
-        curl https://pre-commit.com/install-local.py | python -
-
-        ## Install with the hooks therefore ~/.cache/pre-commit will be created with the repos
-        pre-commit install --install-hooks
-
         ## Search for the repo with the scripts to be added to the PATH
         set +e
         searchFile=\$(find ${newHome}/.cache/pre-commit -type d -name 'scripts' | grep '.ci/scripts')
@@ -65,7 +74,7 @@ def call(Map params = [:]) {
         set -e
         ## Validate the pre-commit for the new changes
         git diff-tree --no-commit-id --name-only -r ${commit} | xargs pre-commit run --files | tee ${reportFileName}
-      """
+      """)
     }
   }
   if(junitFlag) {
