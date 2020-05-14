a90d05224d98488db68428fde6b4fad1e21598dc
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-05-14 14:59:15 +0100

    test: add jar requirement

--- test-infra/beats-ci/test_installed_tools.py
+++ test-infra/beats-ci/test_installed_tools.py
@@ -9,6 +9,10 @@ def test_git_installed(host):
   cmd = host.run("git version")
   assert cmd.rc == 0, "it is required for all the Beats projects"
 
+def test_tar_installed(host):
+  cmd = host.run("tar --version")
+  assert cmd.rc == 0, "it is required for the stashV2 and unstashV2 steps"
+
 def test_vault_installed(host):
   cmd = host.run("vault --version")
   assert cmd.rc == 0, "it is required for all the Beats projects"
--- test-infra/beats-ci/test_windows.py
+++ test-infra/beats-ci/test_windows.py
@@ -3,3 +3,7 @@ import testinfra
 def test_python_installed(host):
   cmd = host.run("python --version")
   assert cmd.rc == 0, "it is required for the Beats"
+
+def test_tar_installed(host):
+  cmd = host.run("tar --version")
+  assert cmd.rc == 0, "it is required for the stashV2 and unstashV2 steps"
