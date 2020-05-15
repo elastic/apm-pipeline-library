9d3859832426293b9dad0e96f163498e6869192e
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-05-15 07:34:54 +0100

    test: fix windows labels beats-ci

--- .ci/validateWorkersBeatsCi.groovy
+++ .ci/validateWorkersBeatsCi.groovy
@@ -60,7 +60,7 @@ pipeline {
         axes {
           axis {
             name 'PLATFORM'
-            values 'ubuntu && immutable', 'worker-c07l34n6dwym', 'worker-c07y20b6jyvy', 'worker-c07ll940dwyl', 'worker-c07y20b9jyvy', 'worker-c07y20b4jyvy', 'worker-c07y20bcjyvy', 'worker-395930', 'worker-0a434dec4bdcd060f', 'windows-immutable && windows-2019', 'windows-immutable && windows-2016', 'windows-immutable && windows-2012', 'windows-immutable && windows-10', 'windows-immutable && windows-8', 'windows-immutable && windows-2008-r2', 'windows-immutable && windows-7', 'windows-immutable-32-bit && windows-7-32-bit'
+            values 'ubuntu && immutable', 'worker-c07l34n6dwym', 'worker-c07y20b6jyvy', 'worker-c07ll940dwyl', 'worker-c07y20b9jyvy', 'worker-c07y20b4jyvy', 'worker-c07y20bcjyvy', 'worker-395930', 'worker-0a434dec4bdcd060f', 'immutable && windows-2019', 'immutable && windows-2016', 'immutable && windows-2012-r2', 'immutable && windows-10', 'immutable && windows-8', 'immutable && windows-2008-r2', 'immutable && windows-7', 'immutable && windows-7-32-bit'
           }
         }
         stages {
