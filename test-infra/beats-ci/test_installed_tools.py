54e10787eda715ca9c8077730cbb2b6f299f8da1
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-05-18 17:20:02 +0100

    [test-infra]: add python

--- test-infra/beats-ci/test_installed_tools.py
+++ test-infra/beats-ci/test_installed_tools.py
@@ -13,6 +13,10 @@ def test_make_installed(host):
   cmd = host.run("make --version")
   assert cmd.rc == 0, "it is required for all the Beats projects"
 
+def test_python_installed(host):
+  cmd = host.run("python --version")
+  assert cmd.rc == 0, "it is required for all the Beats projects"
+
 def test_tar_installed(host):
   cmd = host.run("tar --version")
   assert cmd.rc == 0, "it is required for the stashV2 and unstashV2 steps"
