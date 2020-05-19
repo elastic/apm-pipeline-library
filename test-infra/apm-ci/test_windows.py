2e5557cd0ea4ed104aa55278504e8fb352de3a1e
Victor Martinez <victormartinezrubio@gmail.com>
2020-05-19 16:06:18 +0200

    test-infra: make for windows in apm-ci is not required (#546)

--- test-infra/apm-ci/test_windows.py
+++ test-infra/apm-ci/test_windows.py
@@ -4,10 +4,6 @@ def test_dotnet_installed(host):
   cmd = host.run("dotnet --info")
   assert cmd.rc == 0, "it is required for the apm-agent-dotnet"
 
-def test_make_installed(host):
-  cmd = host.run("make --version")
-  assert cmd.rc == 0, "it is required for all the APM projects"
-
 def test_python_installed(host):
   cmd = host.run("python --version")
   assert cmd.rc == 0, "it is required for the apm-agent-python"
