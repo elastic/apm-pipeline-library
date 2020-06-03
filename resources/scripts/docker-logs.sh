875571b97c61f8d2c9f6e53cc09a633b90c4c9b6
Victor Martinez <victormartinezrubio@gmail.com>
2020-06-03 17:56:14 +0100

    Update resources/scripts/docker-logs.sh
    Co-authored-by: Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>

--- resources/scripts/docker-logs.sh
+++ resources/scripts/docker-logs.sh
@@ -43,4 +43,5 @@ for id in ${DOCKER_IDS}
 do
   docker ps -af id="${id}" --no-trunc &> "${id}-cmd.txt"
   docker logs "${id}" &> "${id}.log" || echo "It is not possible to grab the logs of ${id}"
+  (docker inspect "${id}" &> "${id}-inspect.json" | jq 'del(.[].Config.Env)' || echo "It is not possible to grab the inspect of ${id}"
 done
