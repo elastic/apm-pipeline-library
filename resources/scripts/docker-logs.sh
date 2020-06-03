54bc52a9826b58027c53bc9822a874610284506e
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-06-03 14:21:43 +0100

    ci: avoid docker inspect

--- resources/scripts/docker-logs.sh
+++ resources/scripts/docker-logs.sh
@@ -43,5 +43,4 @@ for id in ${DOCKER_IDS}
 do
   docker ps -af id="${id}" --no-trunc &> "${id}-cmd.txt"
   docker logs "${id}" &> "${id}.log" || echo "It is not possible to grab the logs of ${id}"
-  docker inspect "${id}" &> "${id}-inspect.json" || echo "It is not possible to grab the inspect of ${id}"
 done
