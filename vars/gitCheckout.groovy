e790d8f797931a25083ad6c49cf3e76b1bb500f2
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-06-03 13:07:24 +0100

    fix: serialisation issue

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -81,9 +81,13 @@ def call(Map params = [:]){
   dir("${basedir}"){
     if(customised && isDefaultSCM(branch)){
       log(level: 'INFO', text: "gitCheckout: Checkout SCM ${env.BRANCH_NAME} with some customisation.")
+      log(level: 'DEBUG', text: "gitCheckout: default ${scm.extensions.toString()}")
+      log(level: 'DEBUG', text: "gitCheckout: customised ${extensions.toString()}")
+      def mergedExtensions = mergeExtensions(scm.extensions, extensions)
+      log(level: 'DEBUG', text: "gitCheckout: Extensions ${mergedExtensions.toString()}")
       checkout([$class: 'GitSCM', branches: scm.branches,
         doGenerateSubmoduleConfigurations: scm.doGenerateSubmoduleConfigurations,
-        extensions: mergeExtensions(scm.extensions, extensions),
+        extensions: mergedExtensions,
         submoduleCfg: scm.submoduleCfg,
         userRemoteConfigs: scm.userRemoteConfigs])
       fetchPullRefs()
@@ -189,12 +193,26 @@ def setOrgRepoEnvVariables(params) {
 }
 
 @NonCPS
-def mergeExtensions(defaultExtensions, customisedExtensions) {
-  def extensions = customisedExtensions
-  defaultExtensions.each { extension ->
-    if (!customisedExtensions.find { extension.toString().contains(it.get('$class')) }) {
-      extensions << extension
+def mergeExtensions(extensions, customisedExtensions) {
+  def merged = customisedExtensions
+  for(int i = 0; i < extensions.size(); i++){
+    def extension = extensions.get(i)
+    def extensionName = extension.toString()
+    if (qualified(extensionName, customisedExtensions)) {
+      merged << extension
     }
   }
-  return extensions
+  return merged
+}
+
+@NonCPS
+def qualified(value, customisedExtensions) {
+  for(int j = 0; j < customisedExtensions.size(); j++){
+    def custom = customisedExtensions.get(j)
+    def customName = custom.get('$class')
+    if (!value.contains(customName)) {
+      return true
+    }
+  }
+  return false
 }
