2fa3d09420f3f4d153fc219d354d3fe8f37a7b02
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-06-04 15:26:17 +0100

    test-infra: docker experimental client side

--- test-infra/apm-ci/test_installed_tools_docker.py
+++ test-infra/apm-ci/test_installed_tools_docker.py
@@ -10,5 +10,5 @@ def test_docker_compose_installed(host):
   assert cmd.rc == 0, "it is required for all the APM projects"
 
 def test_docker_experimental_configured(host):
-  cmd = "docker version -f '{{.Server.Experimental}}'"
+  cmd = "docker version -f '{{.Client.Experimental}}'"
   assert host.check_output(cmd) == "true", "it is required for building the ARM docker images in the Beats project"
