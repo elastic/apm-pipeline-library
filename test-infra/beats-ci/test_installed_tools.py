9ae9404c3980c85b1d3a08ef9f52272028d2d158
Victor Martinez <victormartinezrubio@gmail.com>
2020-06-12 16:30:44 +0100

    test-infra: docker experimental is not enabled for MacOSX (#611)

--- test-infra/beats-ci/test_installed_tools.py
+++ test-infra/beats-ci/test_installed_tools.py
@@ -10,9 +10,12 @@ def test_docker_compose_installed(host):
   assert cmd.rc == 0, "it is required for all the Beats projects"
 
 def test_docker_experimental_configured(host):
-  # HOME should point to the USER home for this validation
-  cmd = "docker version -f '{{.Client.Experimental}}'"
-  assert host.check_output(cmd) == "true", "it is required for building the ARM docker images in the Beats project"
+  if host.system_info.type == 'darwin' :
+    pytest.skip("unsupported configuration")
+  else:
+    # HOME should point to the USER home for this validation
+    cmd = "docker version -f '{{.Client.Experimental}}'"
+    assert host.check_output(cmd) == "true", "it is required for building the ARM docker images in the Beats project"
 
 def test_gvm_installed(host):
   cmd = host.run("gvm --version")
