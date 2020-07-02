217d45208d8ba6ac492fe81f0fe83d6c8bab87ee
Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>
2020-07-02 09:35:05 +0200

    test: sample implementation of some commands (#518)
    * test: sample implementation of some commands
    
    * fix: checkout
    
    * feat: run in any worker
    
    * fix: remove checkout
    
    * test:add some more implementations
    
    * fix: addlabels
    
    * fix: regexp
    
    * fix: toLowerCase
    
    * fix: regexp
    
    * fix: string replacements
    
    * fix: regexp
    
    * fix: regexp
    
    * fix: variable
    
    * fix: is a list
    
    * fix: remove the @
    
    * text: remove checkout
    
    * test: log the command
    
    * test: log the command
    
    * fix: the checkout is needed
    
    * fix: we need the default too
    
    * test: close and open
    
    * docs: document unssuported things
    
    * fix: post cleanup
    
    * fix: move the post stage
    
    * fix: move the post stage
    
    * Update .ci/chatops.groovy
    
    * Update .ci/chatops.groovy
    
    * Update .ci/chatops.groovy
    
    * Update .ci/chatops.groovy
    
    * Update .ci/chatops.groovy
    
    * fix: commands in alphabetical order
    
    * fx: roder functions
    
    * fix: triggered only on comments
    
    * fix: roder switch

--- .ci/chatops.groovy
+++ .ci/chatops.groovy
@@ -35,7 +35,8 @@ pipeline {
     durabilityHint('PERFORMANCE_OPTIMIZED')
   }
   triggers {
-    issueCommentTrigger('(?i)^\\/run(-\\w+)?$')
+    // most of then come from https://prow.k8s.io/command-help
+    issueCommentTrigger('(?i)^\\/(run|test|lgtm|cc|assign|approve|meow|woof|bark|this-is-|lint|help|hold|label|close|reopen|skip|ok-to-test|package|build|deploy)(-\\w+)?(\\s+.*)?$')
   }
   parameters {
     string(name: 'branch_specifier', defaultValue: "master", description: "the Git branch specifier to build")
@@ -45,17 +46,16 @@ pipeline {
      Checkout the code and stash it, to use it on other stages.
     */
     stage('Checkout') {
-      options { skipDefaultCheckout() }
-      agent { label 'linux && immutable' }
+      agent any
       when {
         beforeAgent true
-        not {
-          triggeredBy 'SCMTrigger'
+        expression {
+          return isCommentTrigger()
         }
       }
       steps {
         deleteDir()
-        checkout scm
+        gitCheckout(basedir: "${BASE_DIR}", shallow: true, depth: 1)
         /*
         gitCheckout(basedir: "${BASE_DIR}", branch: "${params.branch_specifier}",
           repo: "git@github.com:elastic/${env.REPO}.git",
@@ -63,50 +63,262 @@ pipeline {
           githubNotifyFirstTimeContributor: false,
           reference: "/var/lib/jenkins/${env.REPO}.git")
         */
-        matcher()
+        dir("${BASE_DIR}"){
+          matcher()
+        }
+      }
+      post {
+        cleanup {
+          deleteDir()
+        }
       }
     }
   }
 }
 
 def matcher(){
-  switch ("${env.GITHUB_COMMENT}") {
+  echo "Checking command... '${env.GITHUB_COMMENT}'"
+  switch ("${env.GITHUB_COMMENT}".toLowerCase()) {
+    case ~/\/approve/:
+      approve()
+      break
+    case ~/\/assign.*/:
+      assign()
+      break
+    case ~/\/build/:
+      build()
+      break
+    case ~/\/cc.*/:
+      ccCmd()
+      break
+    case ~/\/close/:
+      closeCmd()
+      break
+    case ~/\/deploy/:
+      deploy()
+      break
     case ~/\/run/:
-        runCmd()
-        break
-    case ~/\/run-lint/:
-        lint()
-        break
-    case ~/\/run-test/:
-        test()
-        break
-    case ~/\/run-build/:
-        build()
-        break
-    case ~/\/run-deploy/:
-        deploy()
-        break
+      runCmd()
+      break
+    case ~/\/test/:
+      test()
+      break
+    case ~/\/lgtm/:
+      lgtm()
+      break
+    case ~/\/help/:
+      help()
+      break
+    case ~/\/hold.*/:
+      hold()
+      break
+    case ~/\/label.*/:
+      labelCmd()
+      break
+    case ~/\/lint/:
+      lint()
+      break
+    case ~/\/meow/:
+      meow()
+      break
+    case ~/\/ok-to-test/:
+      okToTest()
+      break
+    case ~/\/package/:
+      packageCmd()
+      break
+    case ~/\/reopen/:
+      reopenCmd()
+      break
+    case ~/\/skip.*/:
+      skipCmd()
+      break
+    case ~/\/woof/:
+    case ~/\/bark/:
+      woof()
+      break
+    case ~/\/this-is-.*/:
+      thisIs()
+      break
     default:
-        echo "Unrecognized..."
+      echo "Unrecognized command... '${env.GITHUB_COMMENT}'"
   }
 }
 
-def runCmd(){
+def approve(){
   echo "${env.GITHUB_COMMENT}"
+  //TODO not implemented in https://github.com/jenkinsci/pipeline-github-plugin
+  // see https://github.com/jenkinsci/pipeline-github-plugin/pull/37
+  // pullRequest.review('APPROVE')
+  echo "Unsupported"
+}
+
+def assign(){
+  echo "${env.GITHUB_COMMENT}"
+  def usr = "${env.GITHUB_COMMENT}"
+  usr -= 'cancel'
+  usr -= '/assign'
+  usr -= '@'
+  usr = usr.trim()
+  if(GITHUB_COMMENT.contains('cancel')){
+    pullRequest.removeAssignees([usr])
+  } else {
+    pullRequest.addAssignees([usr])
+  }
+}
+
+def build(){
+  echo "${env.GITHUB_COMMENT}"
+  build(job: 'apm-shared/apm-pipeline-library-mbp/master',
+    parameters: [
+      string(name: 'MAVEN_CONFIG', value: ''),
+      booleanParam(name: 'make_release', value: false)
+    ],
+    propagate: false,
+    quietPeriod: 10,
+    wait: false
+  )
+}
+
+def ccCmd(){
+  echo "${env.GITHUB_COMMENT}"
+  def usr = "${env.GITHUB_COMMENT}"
+  usr -= 'cancel'
+  usr -= '/cc'
+  usr -= '@'
+  usr = usr.trim()
+  if(GITHUB_COMMENT.contains('cancel')){
+    pullRequest.deleteReviewRequests([usr])
+  } else {
+    pullRequest.createReviewRequests([usr])
+  }
+}
+
+def closeCmd(){
+  echo "${env.GITHUB_COMMENT}"
+  pullRequest.setState('closed')
+}
+
+def deploy(){
+  echo "${env.GITHUB_COMMENT}"
+}
+
+def help(){
+  echo "${env.GITHUB_COMMENT}"
+  def body = """
+  # ChatOps commands Help
+  * **/approve** - Approve the PR.
+  * **/assign [cancel] @Someone** - Assing the PR to Someone.
+  * **/bark** - Add a dog image to the issue or PR.
+  * **/build** - Launch the build process.
+  * **/cc [cancel] @Someone** - Requests a review from the user(s).
+  * **/close** - Close the PR.
+  * **/deploy** - Launch the deploy process.
+  * **/help** - Punt a comment with the ChatOps commands
+  * **/hold [cancel]** - Adds or removes the `do-not-merge/hold` Label which is used to indicate that the PR should not be automatically merged.
+  * **/label [cancel]** labelName - Adds a label to the PR
+  * **/lgtm [cancel]** - Adds or removes the 'lgtm' label which is typically used to gate merging.
+  * **/lint** - Launch the linting proces.
+  * **/meow** - Add a cat image to the issue or PR.
+  * **/ok-to-test** - Marks a PR as 'trusted' and starts tests.
+  * **/package** - Launch the packaging proces.
+  * **/reopen** - Reopen the PR.
+  * **/run [something]** - Launch the "something" process
+  * **/skip [cancel]** - Mark the PR to be skipped from build in CI.
+  * **/test** - Launch the test process.
+  * **/this-is-{fine|not-fine|unbearable}** - Add a reaction image to the PR.
+  * **/woof** - Add a dog image to the issue or PR.
+  """
+  pullRequest.comment(body)
+}
+
+def hold(){
+  echo "${env.GITHUB_COMMENT}"
+  if(env.GITHUB_COMMENT.contains('cancel')){
+    pullRequest.removeLabel("DO-NO-MERGGE")
+  } else {
+    pullRequest.addLabels(["DO-NO-MERGGE"])
+  }
+}
+
+def lgtm(){
+  echo "${env.GITHUB_COMMENT}"
+  pullRequest.addLabels(["LGTM"])
 }
 
 def lint(){
   echo "${env.GITHUB_COMMENT}"
 }
 
+def meow(){
+  echo "${env.GITHUB_COMMENT}"
+  def body = """
+  ![image](https://media.giphy.com/media/v6aOjy0Qo1fIA/giphy.gif)
+  """
+  pullRequest.comment(body)
+}
+
+def labelCmd(){
+  echo "${env.GITHUB_COMMENT}"
+  def label = "${env.GITHUB_COMMENT}"
+  label -= 'cancel'
+  label -= '/label'
+  label = label.trim()
+  if(env.GITHUB_COMMENT.contains('cancel')){
+    pullRequest.removeLabel(label)
+  } else {
+    pullRequest.addLabels([label])
+  }
+}
+
+def okToTest(){
+  echo "${env.GITHUB_COMMENT}"
+  if(env.GITHUB_COMMENT.contains('cancel')){
+    pullRequest.removeLabel("OK-TO-TEST")
+  } else {
+    pullRequest.addLabels(["OK-TO-TEST"])
+  }
+}
+
+def packageCmd(){
+  echo "${env.GITHUB_COMMENT}"
+}
+
+def reopenCmd(){
+  echo "${env.GITHUB_COMMENT}"
+  // It does not work because closed jobs are not triggered by comments.
+  echo "Unsupported"
+}
+
+def runCmd(){
+  echo "${env.GITHUB_COMMENT}"
+}
+
+def skipCmd(){
+  echo "${env.GITHUB_COMMENT}"
+  if(env.GITHUB_COMMENT.contains('cancel')){
+    pullRequest.removeLabel("SKIP-CI")
+  } else {
+    pullRequest.addLabels(["SKIP-CI"])
+  }
+}
+
 def test(){
   echo "${env.GITHUB_COMMENT}"
 }
 
-def build(){
+def thisIs(){
   echo "${env.GITHUB_COMMENT}"
+  def body = """
+  ![image](https://media.giphy.com/media/v6aOjy0Qo1fIA/giphy.gif)
+  """
+  pullRequest.comment(body)
 }
 
-def deploy(){
+def woof(){
   echo "${env.GITHUB_COMMENT}"
+  def body = """
+  ![image](https://media.giphy.com/media/4Zo41lhzKt6iZ8xff9/giphy.gif)
+  """
+  pullRequest.comment(body)
 }
