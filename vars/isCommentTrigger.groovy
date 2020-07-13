b36f197462ea8ec41118c9342cc1bea96b798af2
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-07-13 17:52:14 +0100

    print error message

--- vars/isCommentTrigger.groovy
+++ vars/isCommentTrigger.groovy
@@ -37,8 +37,8 @@ def call(){
                                              url: "https://api.github.com/orgs/elastic/members/${env.BUILD_CAUSE_USER}")
       // githubApiCall returns either a raw ouput or an error message if so it means the user is not a member.
       found = membershipResponse.message?.trim() ? false : true
-    } catch(e) {
-      log(level: 'DEBUG', text: 'isCommentTrigger: only users under the elastic organisation are allowed.')
+    } catch(err) {
+      log(level: 'DEBUG', text: "isCommentTrigger: only users under the elastic organisation are allowed. Message: See ${err.toString()}")
       // Then it means 404 errorcode.
       // See https://developer.github.com/v3/orgs/members/#response-if-requester-is-an-organization-member-and-user-is-not-a-member
       found = false
