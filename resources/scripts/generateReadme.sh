5d4aa66f28a63e9594b26266541153a9147f4eb0
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-07-27 16:16:14 +0100

    fix lint defects for the javascript one

--- resources/scripts/generateReadme.sh
+++ resources/scripts/generateReadme.sh
@@ -30,12 +30,14 @@ README="${FOLDER}/README.md"
 echo "FOLDER=${1}"
 echo "README=${FOLDER}/README.md"
 
-echo "# Steps Documentation" > ${README}
-for i in ${FOLDER}/*.txt
+echo "# Steps Documentation" > "${README}"
+for i in "${FOLDER}"/*.txt
 do
-  echo "Procesing $i"
-  step=$(basename $i .txt)
-  echo "## ${step}" >>  ${README}
-  cat $i >>  ${README}
-  echo "" >> ${README}
+  echo "Procesing ${i}"
+  step=$(basename "${i}" .txt)
+  {
+    echo "## ${step}"
+    cat "$i"
+    echo ""
+  } >> "${README}"
 done
