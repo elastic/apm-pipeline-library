617072d347bd3b0eeebf444e982126f5ef1052f3
Ivan Fernandez Calvo <kuisathaverat@gmail.com>
2020-09-14 13:26:30 +0200

    chore: license and linting

--- resources/scripts/apm-cli/pytest_apm/setup.py
+++ resources/scripts/apm-cli/pytest_apm/setup.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http:www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
 
 from setuptools import setup
 
@@ -7,7 +23,7 @@ setup(
     version='0.0.1',
     py_modules=["pytest_apm"],
     entry_points={"pytest11": ["apm = pytest_apm"]},
-    install_requires=["setuptools>=40.0", "pytest >= 5.0"],
+    install_requires=["setuptools>=40.0", "pytest >= 5.0", "elastic-apm >= 5.7"],
     python_requires=">=3.5",
     url='https://github.com/elastic/apm-pipeline-library/tree/master/resources/scripts/apm-cli',
     license='Apache License Version 2.0',
