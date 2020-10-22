c1baf4693f6aa97745a4c8bb90e13abfab254b87
Victor Martinez <victormartinezrubio@gmail.com>
2020-10-22 14:12:56 +0100

    [test-infra] gh not validated on Windows (#788)

--- test-infra/beats-ci/test_beats_windows.py
+++ test-infra/beats-ci/test_beats_windows.py
@@ -1,12 +1,4 @@
 import testinfra
-import pytest
-
-def test_gh_installed(host):
-  if host.system_info.arch == "x86_64" :
-    cmd = host.run("gh --version")
-    assert cmd.rc == 0, "it is required for the notifyBuildReport"
-  else :
-    pytest.skip("unsupported configuration")
 
 def test_make_installed(host):
   cmd = host.run("make --version")
