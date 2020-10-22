5683a67e5fb51150898f71ebd7bb8dc6590567bb
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-10-22 13:23:07 +0100

    gh is not supported for windows in the buildNotifyResult yet

--- test-infra/apm-ci/test_windows.py
+++ test-infra/apm-ci/test_windows.py
@@ -1,5 +1,4 @@
 import testinfra
-import pytest
 
 def test_dotnet_installed(host):
   cmd = host.run("dotnet --info")
@@ -12,10 +11,3 @@ def test_python_installed(host):
 def test_tar_installed(host):
   cmd = host.run("tar --version")
   assert cmd.rc == 0, "it is required for the stashV2 and unstashV2 steps"
-
-def test_gh_installed(host):
-  if host.environment().get('ProgramFiles(x86)') :
-    cmd = host.run("gh --version")
-    assert cmd.rc == 0, "it is required for the notifyBuildReport"
-  else :
-    pytest.skip("unsupported configuration")
