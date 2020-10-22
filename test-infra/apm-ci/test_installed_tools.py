bd328c77666f96b56ff440deed0b22882b981675
Victor Martinez <victormartinezrubio@gmail.com>
2020-10-22 11:15:48 +0100

    [test-infra] gh validation (#777)

--- test-infra/apm-ci/test_installed_tools.py
+++ test-infra/apm-ci/test_installed_tools.py
@@ -9,6 +9,13 @@ def test_gvm_installed(host):
   cmd = host.run("gvm --version")
   assert cmd.rc == 0, "it is required for the apm-agent-go and apm-server"
 
+def test_gh_installed(host):
+  if host.check_output("uname -m") == "x86_64" :
+    cmd = host.run("gh --version")
+    assert cmd.rc == 0, "it is required for the notifyBuildReport"
+  else:
+    pytest.skip("unsupported configuration")
+
 def test_git_installed(host):
   cmd = host.run("git version")
   assert cmd.rc == 0, "it is required for all the APM projects"
