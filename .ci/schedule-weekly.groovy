e0df3f8e67636302035e5da66460c0771c3de4bd
Victor Martinez <victormartinezrubio@gmail.com>
2020-12-03 11:06:49 +0000

    [CI] runbld is now deprecated (#869)

--- .ci/schedule-weekly.groovy
+++ .ci/schedule-weekly.groovy
@@ -40,7 +40,7 @@ pipeline {
     stage('Top failing Beats tests - last 7 days') {
       steps {
         setEnvVar('YYYY_MM_DD', new Date().format("yyyy-MM-dd", TimeZone.getTimeZone('UTC')))
-        runWatcher(watcher: '17635395-61cd-439a-963d-8e7bb6ab22b7', subject: "${env.YYYY_MM_DD}: Top failing Beats tests - last 7 days", sendEmail: true, to: 'victor.martinez@elastic.co')
+        runWatcher(watcher: '17635395-61cd-439a-963d-8e7bb6ab22b7', subject: "${env.YYYY_MM_DD}: Top failing Beats tests - last 7 days", sendEmail: true, to: 'beats-contrib@elastic.co')
       }
     }
   }
