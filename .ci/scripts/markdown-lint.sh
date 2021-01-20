7fe04e04de8097cee5b0fd02ba1e893a58e3b5ab
Manuel de la Peña <social.mdelapenya@gmail.com>
2021-01-20 09:24:16 +0100

    chore: remove log entry

--- .ci/scripts/markdown-lint.sh
+++ .ci/scripts/markdown-lint.sh
@@ -5,6 +5,5 @@ docker pull "${IMAGE}" > /dev/null || true
 
 for f in **/*.md
 do
-    echo "Processing $f file..."
     docker run --rm -t -v "${PWD}:/markdown:ro" -u "$(id -u):$(id -g)" "${IMAGE}" "$@" "/markdown/$f"
 done
