8c7d66416e4f94ab7a79abe9090bb4721705b6b5
Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>
2021-02-08 17:12:24 +0100

    test: taest jobDSL job when them changed (#966)
    * test: taest jobDSL job when them changed
    
    * test: remove script, fix step name
    
    * feat: report junit for jobDSL

--- .ci/jobDSL/jobs/customKibana.groovy
+++ .ci/jobDSL/jobs/customKibana.groovy
@@ -15,9 +15,9 @@
 // specific language governing permissions and limitations
 // under the License.
 
-pipelineJob("apm-shared/oblt-test-env/custom_kibana") {
-  displayName('Job apm-shared/oblt-test-env/custom_kibana')
-  description('Job apm-shared/oblt-test-env/custom_kibana description')
+pipelineJob("apm-shared/oblt-test-env/custom-kibana") {
+  displayName('Job apm-shared/oblt-test-env/custom-kibana')
+  description('Job apm-shared/oblt-test-env/custom-kibana description')
   parameters {
     stringParam("branch_specifier", "master", "the Git branch specifier to build.")
   }
