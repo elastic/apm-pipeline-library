a7c77b63d9272a8e8db855f137e9350511e03ebd
Victor Martinez <VictorMartinezRubio@gmail.com>
2021-02-11 14:18:57 +0000

    REmove imports

--- src/test/groovy/ReleaseNotificationStepTests.groovy
+++ src/test/groovy/ReleaseNotificationStepTests.groovy
@@ -17,8 +17,6 @@
 
 import org.junit.Before
 import org.junit.Test
-import static org.junit.Assert.assertFalse
-import static org.junit.Assert.assertNotNull
 import static org.junit.Assert.assertNull
 import static org.junit.Assert.assertTrue
 
