9c88a7e4f8dc3891f491b4628d22cabf2b1d2f54
kuisathaverat <kuisathaverat@yahoo.com>
2021-02-11 17:28:22 +0100

    test: user parameter

--- .ci/jobDSL/jobs/apm-ci/apm-shared/oblt-test-env/custom_kibana_deploy.groovy
+++ .ci/jobDSL/jobs/apm-ci/apm-shared/oblt-test-env/custom_kibana_deploy.groovy
@@ -19,11 +19,7 @@ pipelineJob("apm-shared/oblt-test-env/custom-kibana-deploy") {
   displayName('Custom Kibana - Deploy')
   description('Job to deploy Custom Kibana deployments')
   parameters {
-    gitParam("branch_specifier"){
-      description "the Git branch specifier to build."
-      type 'BRANCH'
-      defaltValue "*/master"
-    }
+    stringParam("branch_specifier", "master", "the Git branch specifier to build.")
   }
   disabled(false)
   quietPeriod(10)
@@ -41,7 +37,7 @@ pipelineJob("apm-shared/oblt-test-env/custom-kibana-deploy") {
             github("elastic/observability-test-environments", "ssh")
             credentials("f6c7695a-671e-4f4f-a331-acdce44ff9ba")
           }
-          branch("${branch_specifier}")
+          branch "$branch_specifier"
           extensions {
             wipeOutWorkspace()
           }
