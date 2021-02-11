f8934087986d22ece95c6693b0c7fb4614bd3560
kuisathaverat <kuisathaverat@yahoo.com>
2021-02-11 17:05:05 +0100

    test: user parameter

--- .ci/jobDSL/jobs/apm-ci/apm-shared/oblt-test-env/custom_kibana_deploy.groovy
+++ .ci/jobDSL/jobs/apm-ci/apm-shared/oblt-test-env/custom_kibana_deploy.groovy
@@ -37,7 +37,7 @@ pipelineJob("apm-shared/oblt-test-env/custom-kibana-deploy") {
             github("elastic/observability-test-environments", "ssh")
             credentials("f6c7695a-671e-4f4f-a331-acdce44ff9ba")
           }
-          branch('$branch_specifier')
+          branch('${branch_specifier}')
           extensions {
             wipeOutWorkspace()
           }
