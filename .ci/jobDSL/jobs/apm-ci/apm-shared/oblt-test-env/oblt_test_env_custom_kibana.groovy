0f8e1571bd7b3509f41182266ebe148acebb6a9b
kuisathaverat <kuisathaverat@yahoo.com>
2021-02-11 11:54:37 +0100

    fix: do not change type

--- .ci/jobDSL/jobs/apm-ci/apm-shared/oblt-test-env/oblt_test_env_custom_kibana.groovy
+++ .ci/jobDSL/jobs/apm-ci/apm-shared/oblt-test-env/oblt_test_env_custom_kibana.groovy
@@ -15,37 +15,157 @@
 // specific language governing permissions and limitations
 // under the License.
 
-//DELETE this job
-pipelineJob("apm-shared/oblt-test-env/oblt-test-env-custom-kibana") {
-  displayName('Custom Kibana - Deploy')
-  description('Job to deploy Custom Kibana deployments')
-  parameters {
-    stringParam("branch_specifier", "master", "the Git branch specifier to build.")
+multibranchPipelineJob('apm-shared/oblt-test-env/oblt-test-env-custom-kibana') {
+  primaryView('All')
+  displayName('Job apm-shared/oblt-test-env/oblt-test-env-custom-kibana')
+  description('Job apm-shared/oblt-test-env/oblt-test-env-custom-kibana description')
+  orphanedItemStrategy {
+    discardOldItems {
+      numToKeep(20)
+      daysToKeep(7)
+    }
   }
-  disabled(false)
-  quietPeriod(10)
-  logRotator {
-    numToKeep(10)
-    daysToKeep(7)
-    artifactNumToKeep(10)
-    artifactDaysToKeep(-1)
-  }
-  definition {
-    cpsScm {
-      scm {
-        git {
-          remote {
-            github("elastic/observability-test-environments", "ssh")
-            credentials("f6c7695a-671e-4f4f-a331-acdce44ff9ba")
-          }
-          branch('*${branch_specifier}')
-          extensions {
-            wipeOutWorkspace()
+  branchSources {
+    branchSource {
+      source {
+        github {
+          id('apm-shared/oblt-test-env/oblt-test-env-custom-kibana') // IMPORTANT: use a constant and unique identifier
+          credentialsId('2a9602aa-ab9f-4e52-baf3-b71ca88469c7-UserAndToken')
+          repoOwner('elastic')
+          repository('apm-pipeline-library')
+          repositoryUrl('https://github.com/elastic/apm-pipeline-library.git')
+          configuredByUrl(true)
+          // The behaviours control what is discovered from the GitHub repository.
+          traits {
+            checkoutOptionTrait {
+              extension {
+                // Specify a timeout (in minutes) for checkout.
+                timeout(15)
+              }
+            }
+            cleanBeforeCheckoutTrait {
+              extension {
+                // Deletes untracked submodules and any other subdirectories which contain .git directories.
+                deleteUntrackedNestedRepositories(true)
+              }
+            }
+            cloneOptionTrait {
+              extension {
+                // Perform shallow clone, so that git will not download the history of the project, saving time and disk space when you just want to access the latest version of a repository.
+                shallow(true)
+                // Deselect this to perform a clone without tags, saving time and disk space when you just want to access what is specified by the refspec.
+                noTags(false)
+                // Specify a folder containing a repository that will be used by Git as a reference during clone operations.
+                reference("/var/lib/jenkins//apm-pipeline-library.git")
+                // Specify a timeout (in minutes) for clone and fetch operations.
+                timeout(15)
+                // Set shallow clone depth, so that git will only download recent history of the project, saving time and disk space when you just want to access the latest commits of a repository.
+                depth(5)
+                // Perform initial clone using the refspec defined for the repository.
+                honorRefspec(false)
+              }
+            }
+            // Disables notifications (commit status updates) to GitHub for builds.
+            //disableStatusUpdateTrait()
+            // Discovers other specified refs on the repository.
+            //discoverOtherRefsTrait {
+              // The pattern under /refs on the remote repository to discover, can contain a wildcard.
+              //ref('')
+              // Mapping for how the ref can be named in for example the @Library.
+              //nameMapping('')
+            //}
+            // Discovers branches on the repository.
+            // https://github.com/jenkinsci/github-branch-source-plugin/blob/master/src/main/java/org/jenkinsci/plugins/github_branch_source/BranchDiscoveryTrait.java#L55-L70
+            gitHubBranchDiscovery{
+              // Determines which branches are discovered.
+              strategyId(1)
+            }
+            // Discovers pull requests where the origin repository is the same as the target repository.
+            // https://github.com/jenkinsci/github-branch-source-plugin/blob/master/src/main/java/org/jenkinsci/plugins/github_branch_source/OriginPullRequestDiscoveryTrait.java#L57-L72
+            //gitHubPullRequestDiscovery {
+              // Determines how pull requests are discovered: Merging the pull request with the current target branch revision Discover each pull request once with the discovered revision corresponding to the result of merging with the current revision of the target branch.
+              //strategyId(1)
+            //}
+            //originPullRequestDiscoveryTrait {
+              //strategyId(3) //build both the head and merge refs
+            //}
+            // Discovers tags on the repository.
+            gitHubTagDiscovery()
+            // filers heads
+            headRegexFilter {
+              // A Java regular expression to restrict the names.
+              regex('(master|main|PR-.*|\\d+\\.x)')
+            }
+            // ignore push
+            //ignoreOnPushNotificationTrait()
+            // Defines a custom context label to be sent as part of Github Status notifications for this project.
+            notificationContextTrait {
+              // The text of the context label for Github status notifications.
+              contextLabel('apm-shared/oblt-test-env/oblt-test-env-custom-kibana')
+              // Appends the relevant suffix to the context label based on the build type.
+              typeSuffix(true)
+            }
+            wipeWorkspaceTrait()
           }
         }
+        buildStrategies {
+          buildChangeRequests {
+            // If the change request / pull request is a merge, there are two reasons for a revision change: The origin of the change request may have changed The target of the change request may ha
+            ignoreTargetOnlyChanges(false)
+            // Some sources can permit change request / pull request from external entities.
+            ignoreUntrustedChanges(true)
+          }
+          // Builds regular branches whenever a change is detected.
+          buildRegularBranches()
+          // Builds branches only if none of the sub strategies match.
+          //buildNamedBranches {
+            // The rules to use when matching a branch name.
+            //filters {
+              // Matches the name verbatim.
+              //exact {
+                // The name to match.
+                //name('master')
+                // Check this box if the name should be matched case sensitively.
+                //caseSensitive(true)
+              //}
+              // Matches the name against a regular expression.
+              //regex {
+                // A Java regular expression to restrict the names.
+                //regex('.*/master')
+                // Check this box if the name should be matched case sensitively.
+                //caseSensitive(true)
+              //}
+            //}
+          //}
+          // Builds tags (subject to a configurable tag age time window)
+          buildTags {
+            atLeastDays('-1')
+            // The number of days since the tag was created after which it is no longer eligible for automatic building.
+            atMostDays('7')
+          }
+          // Skip initial build on first branch indexing
+          skipInitialBuildOnFirstBranchIndexing()
+        }
       }
-      lightweight(true)
-      scriptPath(".ci/customKibana.groovy")
+    }
+  }
+  configure {
+    // workaround for JENKINS-46202 (https://issues.jenkins-ci.org/browse/JENKINS-46202)
+    // https://issues.jenkins.io/browse/JENKINS-60874
+    // Discovers pull requests where the origin repository is the same as the target repository.
+    // https://github.com/jenkinsci/github-branch-source-plugin/blob/master/src/main/java/org/jenkinsci/plugins/github_branch_source/OriginPullRequestDiscoveryTrait.java#L57-L72
+    def traits = it / sources / data / 'jenkins.branch.BranchSource' / source / traits
+    traits << 'org.jenkinsci.plugins.github_branch_source.ForkPullRequestDiscoveryTrait' {
+      strategyId 1
+      trust(class: 'org.jenkinsci.plugins.github_branch_source.ForkPullRequestDiscoveryTrait$TrustPermission')
+    }
+    traits << 'org.jenkinsci.plugins.github__branch__source.OriginPullRequestDiscoveryTrait' {
+      strategyId 1
+    }
+  }
+  factory {
+    workflowBranchProjectFactory {
+      scriptPath('.ci/Jenkinsfile')
     }
   }
 }
