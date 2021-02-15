927706211b4519891b8f65ffe19dd36c20435032
Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>
2021-02-15 11:37:57 +0100

    chore: refactor jobs in folders (#974)
    * chore: refactor jobs in folders
    
    * fchore: bump dependencies
    
    * chore: show stacktrace on failures
    
    * fix: support for Jenkins 2.272
    
    * fix: filenames can not contain -
    
    * fix: typo
    
    * fix: do not change type
    
    * fix: dependency issues
    
    * chore: bump gradle version
    
    * chore: remove un-used login
    
    * fix: build folder first
    
    * test: remove configure block
    
    * test: skip tests
    
    * test: reenable configure block
    
    * test: remove configure block
    
    * test: ets exists
    
    * test: use deprecated options
    
    * test: remove jobs
    
    * test: add kustom kibana job
    
    * test: remove jobs
    
    * test: add kustom kibana job
    
    * test: user parameter
    
    * test: user parameter
    
    * test: user parameter
    
    * test: user parameter
    
    * test: user parameter
    
    * test: user parameter
    
    * test: user parameter
    
    * test: user parameter
    
    * test: user parameter
    
    * test: use deprecated options
    
    * test: use deprecated options
    
    * test: use deprecated options
    
    * fix: allow to create mbp
    
    * fix: license for some files
    
    * chore: move and rename test mbp job
    
    * fix: license for some files

--- .ci/jobDSL/src/test/groovy/hudson/CustomJenkinsRule.groovy
+++ .ci/jobDSL/src/test/groovy/hudson/CustomJenkinsRule.groovy
@@ -15,12 +15,19 @@
 // specific language governing permissions and limitations
 // under the License.
 
-folder('apm-shared') {
-  displayName('APM shared folder')
-  description('Folder for shared CI jobs')
-}
-
-folder('apm-shared/oblt-test-env') {
-  displayName('Oblt test Clusters')
-  description('Folder for Oblt test Clusters CI jobs')
+// jenkins-test-harness does not handle detached plugins properly
+// see https://issues.jenkins.io/browse/JENKINS-60295?focusedCommentId=400912&page=com.atlassian.jira.plugin.system.issuetabpanels%3Acomment-tabpanel#comment-400912
+package hudson
+import org.jvnet.hudson.test.JenkinsRule
+import java.util.logging.Logger
+/**
+ * JenkinsRule to use in Job DSL scripts tests
+ */
+class CustomJenkinsRule extends JenkinsRule {
+  private static final Logger LOGGER = Logger.getLogger( CustomJenkinsRule.name )
+    CustomJenkinsRule() {
+        super()
+        LOGGER.info( 'Loading CustomJenkinsRule')
+        this.pluginManager = new CustomPluginManager()
+    }
 }
