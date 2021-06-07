aac8062ccadda5c6b428508029d46d745606ff06
Victor Martinez <VictorMartinezRubio@gmail.com>
2021-06-07 14:03:07 +0100

    [test-infra] test docker is running

--- test-infra/apm-ci/test_installed_tools_docker.py
+++ test-infra/apm-ci/test_installed_tools_docker.py
@@ -13,3 +13,7 @@ def test_docker_experimental_configured(host):
   # HOME should point to the USER home for this validation
   cmd = "docker version -f '{{.Client.Experimental}}'"
   assert host.check_output(cmd) == "true", "it is required for building the ARM docker images in the Beats project"
+
+def test_docker_is_running(host):
+  cmd = host.run("docker run hello-world")
+  assert cmd.rc == 0, "it is required for all the APM projects"
