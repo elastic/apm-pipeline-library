89f7893b35a3ab7396e4938c12e3cea4328e8adc
Victor Martinez <VictorMartinezRubio@gmail.com>
2021-07-08 11:20:30 +0100

    Ignore MacOS with docker issues

--- .ci/validateWorkersBeatsCi.groovy
+++ .ci/validateWorkersBeatsCi.groovy
@@ -73,7 +73,7 @@ pipeline {
               'immutable && windows-7',
               'ubuntu-18',
               'ubuntu-20',
-              'worker-c07c6107jyw0',
+              // 'worker-c07c6107jyw0', 10.15.7 has docker desktop and it gets stalled
               'worker-c07jc1nzdwym',
               'worker-c07l34n6dwym',
               'worker-c07y20b6jyvy',
