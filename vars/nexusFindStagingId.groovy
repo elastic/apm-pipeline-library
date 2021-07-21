a58ce503983abb57679a6d299c7ea069615fb58f
Mike Place <mike.place@elastic.co>
2021-07-21 12:03:19 +0200

    debugging

--- vars/nexusFindStagingId.groovy
+++ vars/nexusFindStagingId.groovy
@@ -50,6 +50,8 @@ def call(Map args = [:]) {
 
   def HttpURLConnection conn
   String stagingURL = Nexus.getStagingURL(url)
+  System.println(username)
+  System.prinln(stagingURL)
   conn = Nexus.createConnection(stagingURL, username, password, "profile_repositories/${stagingProfileId}")
   Nexus.checkResponse(conn, 200)
   Object response = Nexus.getData(conn)
