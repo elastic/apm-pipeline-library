b68f9956b9077a4322ba9f52ac15abf925af295d
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2021-08-31 11:01:22 +0200

    fix: remove build from the daily pipeline.

--- .ci/schedule-daily.groovy
+++ .ci/schedule-daily.groovy
@@ -96,14 +96,6 @@ pipeline {
           propagate: false,
           wait: false
         )
-
-        build(job: 'apm-ui/apm-ui-e2e-tests-mbp/master',
-          parameters: [
-            booleanParam(name: 'FORCE', value: true),
-          ],
-          propagate: false,
-          wait: false
-        )
       }
     }
     stage('Populate GitHub data') {
