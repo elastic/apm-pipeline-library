080015c1b49ec218e8c7fe8bfea497e15b0e70b6
Victor Martinez <victormartinezrubio@gmail.com>
2021-09-06 12:08:10 +0200

    Use release version for metricbeat/filebeat (#1258)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- .ci/bump-stack-release-version.sh
+++ .ci/bump-stack-release-version.sh
@@ -25,14 +25,11 @@ fi
 echo "Update stack with versions ${VERSION_RELEASE} and ${VERSION_DEV}"
 ${SED} -E -e "s#(def releaseVersion) = \"[0-9]+\.[0-9]+\.[0-9]\"#\1 = \"${VERSION_RELEASE}\"#g" vars/stackVersions.groovy
 ${SED} -E -e "s#(def devVersion) = \"[0-9]+\.[0-9]+\.[0-9]\"#\1 = \"${VERSION_DEV}\"#g" vars/stackVersions.groovy
-${SED} -E -e "s#(docker.elastic.co/beats/.*beat:)[0-9]+\.[0-9]+\.[0-9]#\1${VERSION_DEV}#g" .ci/packer_cache.sh
-${SED} -E -e "s#(docker.elastic.co/beats/.*beat:)[0-9]+\.[0-9]+\.[0-9]#\1${VERSION_DEV}#g" vars/filebeat.groovy
-${SED} -E -e "s#(docker.elastic.co/beats/.*beat:)[0-9]+\.[0-9]+\.[0-9]#\1${VERSION_DEV}#g" vars/metricbeat.groovy
+${SED} -E -e "s#(docker.elastic.co/beats/.*beat:)[0-9]+\.[0-9]+\.[0-9]#\1${VERSION_RELEASE}#g" .ci/packer_cache.sh
+${SED} -E -e "s#(docker.elastic.co/beats/.*beat:)[0-9]+\.[0-9]+\.[0-9]#\1${VERSION_RELEASE}#g" vars/filebeat.groovy
+${SED} -E -e "s#(docker.elastic.co/beats/.*beat:)[0-9]+\.[0-9]+\.[0-9]#\1${VERSION_RELEASE}#g" vars/metricbeat.groovy
 
-git add vars/stackVersions.groovy
-git add .ci/packer_cache.sh
-git add vars/filebeat.groovy
-git add vars/metricbeat.groovy
+git add vars/stackVersions.groovy .ci/packer_cache.sh vars/filebeat.groovy vars/metricbeat.groovy
 git diff --staged --quiet || git commit -m "[Automation] Update elastic stack release version to ${VERSION_RELEASE} and ${VERSION_DEV}"
 git --no-pager log -1
 
