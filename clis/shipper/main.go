8ed8fb21b24743a5b3e0c5666d0ec42211288164
Manuel de la Peña <mdelapenya@gmail.com>
2021-09-14 19:09:21 +0200

    chore: handle default steps info

--- clis/shipper/main.go
+++ clis/shipper/main.go
@@ -1,7 +1,211 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"github.com/Jeffail/gabs/v2"
+)
+
+const (
+	DEFAULT_HASH = "{ }"
+	STEPS_ERRORS = "steps-errors.json"
+	STEPS_INFO   = "steps-info.json"
+)
+
+var baseURL string
+var blueOceanURL string
+var jenkinsURL string
+var sharedLibPath string
+
+func init() {
+	jenkinsURL = os.Getenv("JENKINS_URL")
+	blueOceanURL = os.Getenv("BO_JOB_URL")
+	sharedLibPath = os.Getenv("UTILS_LIB")
+
+	baseURL = jenkinsURL
+	if strings.EqualFold(jenkinsURL, "") {
+		index := strings.Index(blueOceanURL, "/blue/rest/")
+		baseURL = string([]rune(blueOceanURL)[0:index])
+	}
+
+	fmt.Printf("JENKINS_URL: %s\n", jenkinsURL)
+	fmt.Printf("BO_JOB_URL: %s\n", blueOceanURL)
+	fmt.Printf("UTILS_LIB: %s\n", sharedLibPath)
+	fmt.Printf("BASE_URL: %s\n", baseURL)
+}
 
 func main() {
-	fmt.Print("Hello shipper!")
+	fmt.Print("Hello shipper!\n")
+
+	url := fmt.Sprintf("%s/steps/?limit=10000", blueOceanURL)
+
+	json, err := fetchAndDefaultStepsInfo(url, DEFAULT_HASH)
+	if err != nil {
+		fmt.Printf(">> %s", err)
+		os.Exit(1)
+	}
+	ioutil.WriteFile(STEPS_INFO, []byte(json.String()), 0644)
+}
+
+func fetch(url string) (*gabs.Container, error) {
+	fmt.Printf("INFO: curl %s\n", url)
+
+	// Let's support retry in the CI.
+	b, err := fileExists(sharedLibPath)
+	if err != nil {
+		return nil, err
+	}
+
+	req := HTTPRequest{
+		URL: url,
+	}
+
+	times := 1
+
+	if !strings.EqualFold(jenkinsURL, "") && b {
+		times = 3
+	}
+
+	var json *gabs.Container
+
+	for i := 0; i < times; i++ {
+		response, err := Get(req)
+		if err != nil {
+			if i == times-1 {
+				return nil, err
+			}
+
+			if times > 1 {
+				fmt.Printf(">> Retrying (%d)! %s\n", (times - i), err)
+				continue
+			}
+		}
+
+		defer response.Close()
+		json, err = gabs.ParseJSONBuffer(response)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return json, nil
+}
+
+func fetchAndDefault(url string, defaultValue string) (*gabs.Container, error) {
+	json, err := fetch(url)
+	if err != nil {
+		return nil, err
+	}
+
+	if json == nil {
+		// valid response but empty
+		json, err = gabs.ParseJSON([]byte(defaultValue))
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return json, nil
+}
+
+func fetchAndDefaultStepsInfo(url string, hash string) (*gabs.Container, error) {
+	json, err := fetchAndDefault(url, hash)
+	if err != nil {
+		return nil, err
+	}
+
+	// Prepare steps errors report
+	stepsErrors := gabs.New()
+
+	// create an array at root dir
+	stepsErrors, err = stepsErrors.Array()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, step := range json.Children() {
+		if step.Path("result").Data().(string) == "FAILURE" {
+			if step.Path("type").Data().(string) == "STEP" && strings.EqualFold(step.Path("displayDescription").Data().(string), "") {
+				/*
+					Update the displayDescription for those steps with a failure and an empty displayDescription.
+					For instance, when using the pipeline step `error('foo')`
+					then the 'foo' message is not shown in the BlueOcean restAPI.
+				*/
+				linksSelfHref := step.Path("_links.self.href")
+				req := HTTPRequest{
+					URL: fmt.Sprintf("%s%slog/", baseURL, linksSelfHref.Data().(string)),
+				}
+
+				response, err := Get(req)
+				/*
+					If the URL was unreachable then the file won't exist.
+					For such use case, then avoid any transformation.
+				*/
+				if err == nil {
+					defer response.Close()
+
+					lines := []string{}
+					rd := bufio.NewReader(response)
+					for {
+						line, err := rd.ReadString('\n')
+						if err != nil {
+							return nil, err
+						}
+
+						lines = append(lines, line)
+						if len(lines) == 100 {
+							// only retrieve 100 first elements from the log
+							break
+						}
+					}
+				}
+			}
+
+			normaliseSteps(step)
+
+			err = stepsErrors.ArrayAppend(step)
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	return stepsErrors, nil
+}
+
+// fileExists checks if a path exists in the file system
+func fileExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return true, err
+}
+
+func normaliseSteps(json *gabs.Container) {
+	linksSelfHref := fmt.Sprintf("%s%slog", baseURL, json.Path("_links.self.href").Data().(string))
+
+	json.Set(linksSelfHref, "url")
+
+	err := json.Delete("_links")
+	if err != nil {
+		fmt.Printf(">> cannot delete _links from JSON object: %s\n", err)
+	}
+
+	err = json.DeleteP("_class")
+	if err != nil {
+		fmt.Printf(">> cannot delete _class from JSON object: %s\n", err)
+	}
+
+	err = json.DeleteP("actions")
+	if err != nil {
+		fmt.Printf(">> cannot delete actions from JSON object: %s\n", err)
+	}
 }
