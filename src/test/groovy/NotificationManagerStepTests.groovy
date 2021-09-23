8a892b1f141ab2512a9673a758aa675364f3cf4e
Victor Martinez <victormartinezrubio@gmail.com>
2021-09-23 12:49:11 +0100

    Exclude errors related to the approval-list and githubPrCheckApproved in the GitHub comment (#1295)

--- src/test/groovy/NotificationManagerStepTests.groovy
+++ src/test/groovy/NotificationManagerStepTests.groovy
@@ -217,6 +217,27 @@ class NotificationManagerStepTests extends ApmBasePipelineTest {
   }
 
   @Test
+  void test_notify_pr_with_aborted_not_allowed_to_run() throws Exception {
+    script.notifyPR(
+      build: readJSON(file: "build-info_aborted_allowed_to_run.json"),
+      buildStatus: "ABORTED",
+      changeSet: readJSON(file: "changeSet-info.json"),
+      statsUrl: "https://ecs.example.com/app/kibana",
+      stepsErrors: readJSON(file: "steps-errors-allowed-to-run.json"),
+      testsErrors: readJSON(file: "tests-errors.json"),
+      testsSummary: readJSON(file: "tests-summary.json")
+    )
+    printCallStack()
+    // Then the description contains the reason, there is no need to report the Error for the githubPrCheckApproved
+    // and load resources for the approval-list should not be reported in the GitHub comment.
+    assertTrue(assertMethodCallContainsPattern('githubPrComment', 'Build Aborted'))
+    assertTrue(assertMethodCallContainsPattern('githubPrComment', '> The PR is not allowed to run in the CI yet'))
+    assertFalse(assertMethodCallContainsPattern('githubPrComment', 'approval-list/elastic'))
+    assertFalse(assertMethodCallContainsPattern('githubPrComment', 'githubPrCheckApproved'))
+    assertJobStatusSuccess()
+  }
+
+  @Test
   void test_notify_pr_with_failure() throws Exception {
     script.notifyPR(
       build: readJSON(file: "build-info.json"),
