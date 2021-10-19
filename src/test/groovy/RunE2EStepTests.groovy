8118c27aba50ac9cb9107c0e66a895f6d6babf5d
Victor Martinez <victormartinezrubio@gmail.com>
2021-10-19 10:43:12 +0100

    Update src/test/groovy/RunE2EStepTests.groovy
    Co-authored-by: Manuel de la Peña <mdelapenya@gmail.com>

--- src/test/groovy/RunE2EStepTests.groovy
+++ src/test/groovy/RunE2EStepTests.groovy
@@ -20,7 +20,7 @@ import org.junit.Test
 import static org.junit.Assert.assertFalse
 import static org.junit.Assert.assertTrue
 
-class RunE2eStepTests extends ApmBasePipelineTest {
+class RunE2EStepTests extends ApmBasePipelineTest {
 
   @Override
   @Before
