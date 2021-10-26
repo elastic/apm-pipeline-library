a83444260a9ec9d018236accfb596a4de3db0f27
Victor Martinez <VictorMartinezRubio@gmail.com>
2021-10-26 13:09:35 +0100

    fix typo in the class name

--- src/test/groovy/GithubTraditionalPrCommentStepTests.groovy
+++ src/test/groovy/GithubTraditionalPrCommentStepTests.groovy
@@ -21,7 +21,7 @@ import org.junit.Test
 import static org.junit.Assert.assertEquals
 import static org.junit.Assert.assertTrue
 
-class githubTraditionalPrCommenttStepTests extends ApmBasePipelineTest {
+class GithubTraditionalPrCommentStepTests extends ApmBasePipelineTest {
 
   def commentInterceptor = [
     id: 1,
