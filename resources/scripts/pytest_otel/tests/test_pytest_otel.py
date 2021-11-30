054ccbd11f840e5822df9cb49fb34afc02095ac7
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2021-11-30 17:37:36 +0100

    feat: tests

--- resources/scripts/pytest_otel/tests/test_pytest_otel.py
+++ resources/scripts/pytest_otel/tests/test_pytest_otel.py
@@ -35,22 +35,28 @@ def testdir(testdir):
         testdir.runpytest = testdir.runpytest_subprocess
     return testdir
 
-def assertTestSuit(span):
+
+def assertTestSuit(span, outcome, status):
     assert span["kind"] == "SpanKind.SERVER"
-    assert span["status"]["status_code"] == "OK"
-    assert span["attributes"]["test.outcome"] == "passed"
+    assert span["status"]["status_code"] == status
+    "OK" or span["status"]["status_code"] == "UNSET"
+    if outcome is not None:
+        assert span["attributes"]["test.outcome"] == "passed"
     assert span["parent_id"] is None
     return True
+
 
 def assertSpan(span, name, outcome, status):
     assert span["kind"] == "SpanKind.INTERNAL"
     assert span["status"]["status_code"] == status
     assert span["attributes"]["test.name"] == name
-    assert span["attributes"]["test.outcome"] == outcome
+    if outcome is not None:
+        assert span["attributes"]["test.outcome"] == outcome
     assert len(span["parent_id"]) > 0
     return True
 
-def assertTest(testdir, name, outcome, status):
+
+def assertTest(testdir, name, ts_outcome, ts_status, outcome, status):
     testdir.runpytest("--otel-span-file-output=./test_spans.json")
     span_list = None
     with open("test_spans.json", encoding='utf-8') as input:
@@ -61,9 +67,10 @@ def assertTest(testdir, name, outcome, status):
         if span["name"] == "Running {}".format(name):
             foundTest = assertSpan(span, name, outcome, status)
         if span["name"] == "Test Suite":
-            foundTestSuit = assertTestSuit(span)
-    assert foundTest
+            foundTestSuit = assertTestSuit(span, ts_outcome, ts_status)
+    assert foundTest or name is None
     assert foundTestSuit
+
 
 def test_basic_plugin(testdir):
     testdir.makepyfile(
@@ -73,7 +80,7 @@ def test_basic():
     time.sleep(5)
     pass
 """)
-    assertTest(testdir, "test_basic", "passed", "OK")
+    assertTest(testdir, "test_basic", "passed", "OK", "passed", "OK")
 
 
 def test_success_plugin(testdir):
@@ -83,7 +90,7 @@ def test_success_plugin(testdir):
 def test_success():
     assert True
 """)
-    assertTest(testdir, "test_success", "passed", "OK")
+    assertTest(testdir, "test_success", "passed", "OK", "passed", "OK")
 
 
 def test_failure_plugin(testdir):
@@ -93,39 +100,48 @@ def test_failure_plugin(testdir):
 def test_failure():
     assert 1 < 0
 """)
-    assertTest(testdir, "test_failure", "failed", "ERROR")
+    assertTest(testdir, "test_failure", "failed", "OK", "failed", "ERROR")
 
-#
-# def test_success():
-#     """Success."""
-#     sleep_random_number()
-#     assert True
-#
-# def test_failure():
-#     """Failure."""
-#     sleep_random_number()
-#     assert 1 < 0
-#
-# def test_failure_code():
-#     """Failure Code."""
-#     sleep_random_number()
-#     d = 1/0
-#     assert True
-#
-# @pytest.mark.skip
-# def test_skip():
-#     """Skip."""
-#     sleep_random_number()
-#     assert True
-#
-# @pytest.mark.xfail
-# def test_xfail():
-#     """XFail."""
-#     sleep_random_number()
-#     assert False
-#
-# @pytest.mark.xfail(run=False)
-# def test_xfail_no_run():
-#     """XFail No Run."""
-#     sleep_random_number()
-#     assert False
+
+def test_failure_code_plugin(testdir):
+    testdir.makepyfile(
+        common_code
+        + """
+def test_failure_code():
+    d = 1/0
+    pass
+""")
+    assertTest(testdir, "test_failure_code", "failed", "OK", "failed", "ERROR")
+
+
+def test_skip_plugin(testdir):
+    testdir.makepyfile(
+        common_code
+        + """
+@pytest.mark.skip
+def test_skip():
+    assert True
+""")
+    assertTest(testdir, None, None, "UNSET", None, None)
+
+
+def test_xfail_plugin(testdir):
+    testdir.makepyfile(
+        common_code
+        + """
+@pytest.mark.xfail
+def test_xfail():
+    assert False
+""")
+    assertTest(testdir, None, None, "UNSET", None, None)
+
+
+def test_xfail_no_run_plugin(testdir):
+    testdir.makepyfile(
+        common_code
+        + """
+@pytest.mark.xfail(run=False)
+def test_xfail_no_run():
+    assert False
+""")
+    assertTest(testdir, None, None, "UNSET", None, None)
