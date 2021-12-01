9cbf59d6ac7c2084c26f7986caebd19218c71971
Victor Martinez <victormartinezrubio@gmail.com>
2021-12-01 13:28:08 +0000

    Support for the AWS cli env (#1427)
    * Initial support for the AWS cli env
    
    * Refactor and add UTs
    
    * Support the credentials and add more traces to the sh steps
    
    * Add ITs
    
    * Fix the csv issue
    
    See https://forums.aws.amazon.com/thread.jspa\?threadID\=328307
    
    * Configure the AWS profile
    
    * Use the right permissions
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ src/test/groovy/WithAWSEnvStepTests.groovy
@@ -0,0 +1,149 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+import org.junit.Before
+import org.junit.Test
+import static org.junit.Assert.assertFalse
+import static org.junit.Assert.assertTrue
+
+class WithAWSEnvStepTests extends ApmBasePipelineTest {
+  def script
+
+  @Override
+  @Before
+  void setUp() throws Exception {
+    super.setUp()
+    helper.registerAllowedMethod('isInstalled', [Map.class], { return true })
+    script = loadScript('vars/withAWSEnv.groovy')
+  }
+
+  @Test
+  void test_missing_secret() throws Exception {
+    testMissingArgument('secret') {
+      script.call() {
+        // NOOP
+      }
+    }
+  }
+
+  @Test
+  void test_with_secret() throws Exception {
+    def ret = false
+    try {
+    script.call(secret: VaultSecret.SECRET_AWS_PROVISIONER.toString()) {
+      ret = true
+    }
+    } catch (e) {
+      println e
+    }
+    printCallStack()
+    assertTrue(ret)
+    assertTrue(assertMethodCallContainsPattern('cmd', 'aws configure import --csv file://'))
+    assertTrue(assertMethodCallContainsPattern('withEnv', 'AWS_PROFILE'))
+    assertTrue(assertMethodCallContainsPattern('sh', 'rm'))
+    assertJobStatusSuccess()
+  }
+
+  @Test
+  void test_with_failed() throws Exception {
+    helper.registerAllowedMethod('sh', [Map.class], { m -> throw new Exception('force a failure') })
+    def result = false
+    try {
+      script.call(secret: VaultSecret.SECRET_AWS_PROVISIONER.toString()) {
+        result = true
+      }
+    } catch(err) {
+      println err
+      // NOOP
+    }
+    printCallStack()
+    assertFalse(result)
+  }
+
+  @Test
+  void test_with_force_installation_and_version_already_installed() throws Exception {
+    helper.registerAllowedMethod('isInstalled', [Map.class], { return true })
+    def ret = false
+    script.call(secret: VaultSecret.SECRET_AWS_PROVISIONER.toString(), version: "2.0.0", forceInstallation: true) {
+      ret = true
+    }
+    printCallStack()
+    assertTrue(assertMethodCallContainsPattern('sh', '2.0.0'))
+    assertTrue(assertMethodCallOccurrences('download', 1))
+    assertTrue(ret)
+    assertJobStatusSuccess()
+  }
+
+  @Test
+  void test_with_force_installation_and_version_not_installed() throws Exception {
+    helper.registerAllowedMethod('isInstalled', [Map.class], { m -> return m.tool.equals('wget') })
+    def ret = false
+    script.call(secret: VaultSecret.SECRET_AWS_PROVISIONER.toString(), version: "2.0.0", forceInstallation: true) {
+      ret = true
+    }
+    printCallStack()
+    assertTrue(assertMethodCallContainsPattern('sh', '2.0.0'))
+    assertTrue(assertMethodCallOccurrences('download', 1))
+    assertTrue(ret)
+    assertJobStatusSuccess()
+  }
+
+  @Test
+  void test_without_force_installation_and_version_not_installed() throws Exception {
+    helper.registerAllowedMethod('isInstalled', [Map.class], { m -> return m.tool.equals('wget') || !m.tool.equals('aws') })
+    def ret = false
+    script.call(secret: VaultSecret.SECRET_AWS_PROVISIONER.toString(), version: "2.0.0", forceInstallation: false) {
+      ret = true
+    }
+    printCallStack()
+    assertTrue(assertMethodCallContainsPattern('sh', '2.0.0'))
+    assertTrue(assertMethodCallOccurrences('download', 1))
+    assertTrue(ret)
+    assertJobStatusSuccess()
+  }
+
+  @Test
+  void test_awsURL_in_windows() throws Exception {
+    helper.registerAllowedMethod('isUnix', [], { false })
+    try {
+      script.awsURL('2.2.2')
+    } catch(err) {
+      println err
+      // NOOP
+    }
+    printCallStack()
+    assertTrue(assertMethodCallContainsPattern('error', 'windows is not supported yet'))
+  }
+
+  @Test
+  void test_awsURL_in_32_bits() throws Exception {
+    helper.registerAllowedMethod('is64', [], { return false })
+    def ret = script.awsURL('2.2.2')
+    printCallStack()
+    assertTrue(ret.contains("linux-x86-2.2.2.zip"))
+    assertJobStatusSuccess()
+  }
+
+  @Test
+  void testWindows() throws Exception {
+    testWindows() {
+      script.call() {
+        // NOOP
+      }
+    }
+  }
+}
