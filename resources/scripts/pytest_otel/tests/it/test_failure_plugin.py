e9096dfb21a696878da1730777ca33f0389a53d6
Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>
2021-12-03 13:39:50 +0100

    feat: publish pytest_otel (#1392)
    * feat: publish pytest_otel
    
    * chore: refactor
    
    * feat: tests
    
    * feat: configure file exporter
    
    * chore: use pytester instead testdir
    
    * chore: refactor
    
    * chore: update ignore
    
    * fix: license
    
    * docs: update README
    
    * feat: update otel collector run
    
    * feat: update clean target
    
    * feat: release target
    
    * feta: docker-compose for testing
    
    * chore: bump version
    
    * fix: typo
    
    * fix: remove email
    
    * fix: update license headers
    
    * docs: documents tests
    
    * runs tests in a subprocess
    
    * remove dependencies
    
    * feat: it tests
    
    * docs: add functions comments
    
    * fix: report correct test suit status
    
    * fix: avoid index out of range errors
    
    * feat: Integration tests
    
    * feat: remove junit files in the clean target
    
    * chore: remove release target

---
+++ resources/scripts/pytest_otel/tests/it/test_failure_plugin.py
@@ -0,0 +1,26 @@
+# Copyright The OpenTelemetry Authors
+# SPDX-License-Identifier: Apache-2.0
+
+import pytest
+from utils import assertTest
+
+pytest_plugins = ["pytester"]
+
+common_code = """
+import os
+import time
+import logging
+import pytest
+
+"""
+
+
+def test_failure_plugin(pytester, otel_service):
+    """test a failed test"""
+    pytester.makepyfile(
+        common_code
+        + """
+def test_failure():
+    assert 1 < 0
+""")
+    assertTest(pytester, "test_failure", "failed", "STATUS_CODE_ERROR", "failed", "STATUS_CODE_ERROR")
