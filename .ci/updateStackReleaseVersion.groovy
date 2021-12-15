cc9f3a025076bed62bf51afe5606f8fb1d777e55
Victor Martinez <VictorMartinezRubio@gmail.com>
2021-12-15 18:18:53 +0000

    manually bump releases

--- .ci/updateStackReleaseVersion.groovy
+++ .ci/updateStackReleaseVersion.groovy
@@ -54,10 +54,10 @@ pipeline {
       steps {
         echo ' TODO: calculate the versions'
         script {
-          releaseVersions[bumpUtils.current6Key()] = '6.8.20'
-          releaseVersions[bumpUtils.current7Key()] = '7.15.2'
-          releaseVersions[bumpUtils.nextMinor7Key()] = '7.16.0'
-          releaseVersions[bumpUtils.nextPatch7Key()] = '7.16.0'
+          releaseVersions[bumpUtils.current6Key()] = '6.8.21'
+          releaseVersions[bumpUtils.current7Key()] = '7.16.1'
+          releaseVersions[bumpUtils.nextMinor7Key()] = '7.16.2'
+          releaseVersions[bumpUtils.nextPatch7Key()] = '7.16.2'
           // TODO: to support the 8.x branches
           releaseVersions[bumpUtils.current8Key()] = '8.0.0'
           releaseVersions[bumpUtils.nextMinor8Key()] = '8.1.0'
