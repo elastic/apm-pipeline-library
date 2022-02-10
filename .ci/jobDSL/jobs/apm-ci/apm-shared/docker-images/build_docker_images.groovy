27cdfe3d7e1f8acd52dddbcfbc9e2ceb617c8d80
Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>
2022-02-10 10:24:25 +0100

    fix: some mistakes (#1536)
    * fix: some mistakes
    
    * fix: use git urls
    
    * fix: avoid interpolations

--- .ci/jobDSL/jobs/apm-ci/apm-shared/docker-images/build_docker_images.groovy
+++ .ci/jobDSL/jobs/apm-ci/apm-shared/docker-images/build_docker_images.groovy
@@ -67,7 +67,7 @@ def dockerImages = [
     tag: 'latest',
     folder: 'tools/apm_proxy/frontend',
     push: true,
-    prepare_script: 'cd ${BASE_DIR}/${params.folder} && git clone git@github.com:haproxytech/spoa-mirror.git'
+    prepare_script: 'cd tools/apm_proxy/frontend && git clone git@github.com:haproxytech/spoa-mirror.git'
   ],
   [
     name: 'apm-proxy-be',
@@ -88,53 +88,53 @@ def dockerImages = [
   ],
   [
     name: 'flakey',
-    repo: 'https://github.com/elastic/observability-dev',
+    repo: 'git@github.com:elastic/observability-dev',
     tag: 'latest',
     push: true,
     folder: "apps/automation/jenkins-toolbox"
   ],
   [
     name: 'flakeyv2',
-    repo: 'https://github.com/elastic/observability-dev',
+    repo: 'git@github.com:elastic/observability-dev',
     tag: 'latest',
     push: true,
     folder: "apps/automation/flaky-test-analyzer"
   ],
   [
     name: 'build-analyzer',
-    repo: 'https://github.com/elastic/observability-dev',
+    repo: 'git@github.com:elastic/observability-dev',
     tag: 'latest',
     push: true,
     folder: "apps/automation/build-analyzer"
   ],
   [
     name: 'rebuild-analyzer',
-    repo: 'https://github.com/elastic/observability-dev',
+    repo: 'git@github.com:elastic/observability-dev',
     tag: 'latest',
     push: true,
     folder: "apps/automation/rebuild-analyzer"
   ],
   [
     name: 'integrations-test-reporter',
-    repo: 'https://github.com/elastic/observability-dev',
+    repo: 'git@github.com:elastic/observability-dev',
     tag: 'latest',
     push: true,
     folder: "apps/automation/integrations/reporter"
   ],
   [
     name: 'slack-bridge-hey-apm',
-    repo: 'https://github.com/elastic/observability-dev',
+    repo: 'git@github.com:elastic/observability-dev',
     tag: 'latest',
     push: true,
     folder: "tools/report-bridge"
   ],
   [
     name: 'obs-jenkins-heartbeat',
-    repo: "https://github.com/elastic/observability-robots.git",
+    repo: "git@github.com:elastic/observability-robots.git",
     tag: 'latest',
     push: true,
     prepare_script: '''
-      cd ${BASE_DIR}/${params.folder}
+      cd apps/beats/heartbeat
       pip3 install pyyaml
       python3 ./generate_heartbeat_configs.py
     ''',
@@ -142,21 +142,21 @@ def dockerImages = [
   ],
   [
     name: "bandstand",
-    repo: 'https://github.com/elastic/observability-dev',
+    repo: 'git@github.com:elastic/observability-dev',
     tag: "latest",
     folder: "apps/automation/bandstand",
     push: true
   ],
   [
     name: "azure-vm-tools",
-    repo: 'https://github.com/elastic/azure-vm-extension',
+    repo: 'git@github.com:elastic/azure-vm-extension',
     tag: "latest",
     folder: ".ci/docker/azure-vm-tools",
     push: true
   ],
   [
     name: 'picklesdoc',
-    repo: 'https://github.com/elastic/observability-robots.git',
+    repo: 'git@github.com:elastic/observability-robots.git',
     tag: 'latest',
     build_script: 'make build',
     push_script: 'make push',
@@ -165,7 +165,7 @@ def dockerImages = [
   ],
   [
     name: 'test-plans',
-    repo: 'https://github.com/elastic/observability-robots.git',
+    repo: 'git@github.com:elastic/observability-robots.git',
     tag: 'latest',
     build_script: 'make build',
     push_script: 'make push',
@@ -177,36 +177,36 @@ def dockerImages = [
   */
   [
     name: "apm-integration-testing",
-    repo: 'https://github.com/elastic/apm-integration-testing.git',
+    repo: 'git@github.com:elastic/apm-integration-testing.git',
     tag: "daily",
     push: true
   ],
   [
     name: "apm-integration-testing-all",  // Compile all the APM ITs Docker images (using -all suffix to be able to use this automation)
-    repo: 'https://github.com/elastic/apm-integration-testing.git',
+    repo: 'git@github.com:elastic/apm-integration-testing.git',
     build_script: "make -C docker all-tests",
     push_script: "make -C docker all-push",
     push: true
   ],
   [
     name: "oracle-instant-client",
-    build_script: '''
+    build_script: """
     IMAGE_TAG = "store/oracle/database-instantclient:12.2.0.1"
-    TAG_CACHE = "${params.registry}/${params.tag_prefix}/database-instantclient:12.2.0.1"
-    docker pull ${IMAGE_TAG}
-    docker tag ${IMAGE_TAG} ${TAG_CACHE}
-    docker push ${TAG_CACHE}
-    '''
+    TAG_CACHE = "${registry}/${prefix}/database-instantclient:12.2.0.1"
+    docker pull \${IMAGE_TAG}
+    docker tag \${IMAGE_TAG} \${TAG_CACHE}
+    docker push \${TAG_CACHE}
+    """
   ],
   [
     name: "weblogic",
-    build_script: '''
+    build_script: """
     IMAGE_TAG = "store/oracle/weblogic:12.2.1.3-dev"
-    TAG_CACHE = "${params.registry}/${params.tag_prefix}/weblogic:12.2.1.3-dev"
-    docker pull ${IMAGE_TAG}
-    docker tag ${IMAGE_TAG} ${TAG_CACHE}
-    docker push ${TAG_CACHE}
-    '''
+    TAG_CACHE = "${registry}/${prefix}/weblogic:12.2.1.3-dev"
+    docker pull \${IMAGE_TAG}
+    docker tag \${IMAGE_TAG} \${TAG_CACHE}
+    docker push \${TAG_CACHE}
+    """
   ]
 ]
 
