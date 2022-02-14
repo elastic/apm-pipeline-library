1df287e3c03af300890ba67bc82388fa8f183c28
Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>
2022-02-14 10:19:19 +0100

    fix: remove deleted jobs (#1546)

--- .ci/schedule-daily.groovy
+++ .ci/schedule-daily.groovy
@@ -47,40 +47,6 @@ pipeline {
           wait: false
         )
 
-        build(job: 'apm-shared/apm-docker-images-pipeline',
-          parameters: [
-            string(name: 'registry', value: 'docker.elastic.co'),
-            string(name: 'tag_prefix', value: 'observability-ci'),
-            string(name: 'secret', value: 'secret/apm-team/ci/docker-registry/prod'),
-            booleanParam(name: 'apm_integration_testing', value: true),
-            booleanParam(name: 'apm_server', value: true),
-            booleanParam(name: 'helm_kubectl', value: true),
-            booleanParam(name: 'nodejs', value: true),
-            booleanParam(name: 'opbot', value: true),
-            booleanParam(name: 'oracle_instant_client', value: true),
-            booleanParam(name: 'python', value: true),
-            booleanParam(name: 'ruby', value: true),
-            booleanParam(name: 'rum', value: true),
-            booleanParam(name: 'weblogic', value: true),
-            string(name: 'branch_specifier', value: 'main')
-          ],
-          propagate: false,
-          wait: false
-        )
-
-        build(job: 'apm-shared/apm-docker-opbeans-pipeline',
-          parameters: [
-            string(name: 'registry', value: 'docker.elastic.co'),
-            string(name: 'tag_prefix', value: 'observability-ci'),
-            string(name: 'version', value: 'daily'),
-            string(name: 'secret', value: "${DOCKERELASTIC_SECRET}"),
-            booleanParam(name: 'opbeans', value: true),
-            string(name: 'branch_specifier', value: 'main')
-          ],
-          propagate: false,
-          wait: false
-        )
-
         build(job: 'apm-shared/bump-stack-version-pipeline',
           parameters: [
             booleanParam(name: 'DRY_RUN_MODE', value: false)
