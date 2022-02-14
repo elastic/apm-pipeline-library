3d74f52ca56cfb6f24e67b558e5d315c24fb7949
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-02-14 18:05:56 +0000

    add missing annotation

--- vars/withOtelEnv.groovy
+++ vars/withOtelEnv.groovy
@@ -15,6 +15,8 @@
 // specific language governing permissions and limitations
 // under the License.
 
+import com.cloudbees.groovy.cps.NonCPS
+
 /**
   Configure the OpenTelemetry Jenkins context to run the body closure.
 
