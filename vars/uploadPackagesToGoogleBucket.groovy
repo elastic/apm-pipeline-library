60cb93128b7c5f868be977df61ac0d476a0e4dcd
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-02-15 16:36:05 +0000

    support recursive copy

--- vars/uploadPackagesToGoogleBucket.groovy
+++ vars/uploadPackagesToGoogleBucket.groovy
@@ -43,7 +43,8 @@ def uploadPackages(Map args = [:]) {
     bucket: args.bucketUri,
     credentialsId: args.credentialsId,
     pattern: args.pattern,
-    sharedPublicly: true
+    sharedPublicly: true,
+    extraFlags: '-r'
   )
 }
 
