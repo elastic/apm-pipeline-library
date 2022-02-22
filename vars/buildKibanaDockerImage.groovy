469b272477f375dedd2d0188efc493abe9ca92b7
Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>
2022-02-22 17:31:34 +0100

    feat: speed up checkouts (#1558)
    * feat: speed up checkouts
    
    * test: include tests
    
    * Update vars/fastCheckout.groovy
    
    Co-authored-by: Victor Martinez <victormartinezrubio@gmail.com>
    
    Co-authored-by: Victor Martinez <victormartinezrubio@gmail.com>

--- vars/buildKibanaDockerImage.groovy
+++ vars/buildKibanaDockerImage.groovy
@@ -52,28 +52,7 @@ def call(Map args = [:]){
 
   log(level: 'DEBUG', text: "Cloning Kibana repository, refspec ${refspec}, into ${baseDir}")
 
-  checkout([$class: 'GitSCM',
-    branches: [[name: "*/${refspec}"]],
-    doGenerateSubmoduleConfigurations: false,
-    extensions: [
-      [$class: 'RelativeTargetDirectory', relativeTargetDir: "${baseDir}"],
-      [$class: 'CheckoutOption', timeout: 15],
-      [$class: 'AuthorInChangelog'],
-      [$class: 'IgnoreNotifyCommit'],
-      [$class: 'CloneOption',
-        depth: depth,
-        noTags: false,
-        reference: referenceRepo,
-        shallow: shallow,
-        timeout: 15
-      ]],
-      submoduleCfg: [],
-      userRemoteConfigs: [[
-        credentialsId: "${credentialsId}",
-        url: "http://github.com/elastic/kibana.git",
-        refspec: '+refs/heads/*:refs/remotes/origin/* +refs/pull/*/head:refs/remotes/origin/PR/*',
-        ]]
-  ])
+  fastCheckout(baseDir: baseDir, reference: referenceRepo, credentialsId: credentialsId, depth: depth, shallow: true, url: 'http://github.com/elastic/kibana.git', refspec: refspec)
 
   def kibanaVersion = ''
 
