a345d73dfdbb6577988b9699e4ff98516db5d262
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-04-11 13:51:29 +0100

    isCommentTrigger: support for write and org member

--- vars/isCommentTrigger.groovy
+++ vars/isCommentTrigger.groovy
@@ -15,30 +15,53 @@
 // specific language governing permissions and limitations
 // under the License.
 
-import com.cloudbees.groovy.cps.NonCPS
 /**
   Check if the build was triggered by a comment in GitHub.
 
   def commentTrigger = isCommentTrigger()
 */
 
+// @deprecated. For backward compatibility only
 def call(){
+  return call([:])
+}
+
+def call(Map args){
+  def author = args.get('author', env.GITHUB_COMMENT_AUTHOR)
+  def comment = args.get('comment', env.GITHUB_COMMENT)
+  def repo = args.get('repository', env.REPO_NAME)
   def found = false
-  if (env.GITHUB_COMMENT_AUTHOR && env.GITHUB_COMMENT) {
+  if (author && comment) {
     log(level: 'DEBUG', text: 'isCommentTrigger: only users under the elastic organisation are allowed.')
     def token = getGithubToken()
-
-    try {
-      def membershipResponse = githubApiCall(token: token, allowEmptyResponse: true,
-                                            url: "https://api.github.com/orgs/elastic/members/${env.GITHUB_COMMENT_AUTHOR}")
-      // githubApiCall returns either a raw output or an error message if so it means the user is not a member.
-      found = membershipResponse.message?.trim() ? false : true
-    } catch(err) {
-      log(level: 'WARN', text: "isCommentTrigger: only users under the Elastic organisation are allowed. Message: See ${err.toString()}")
-      // Then it means 404 errorcode.
-      // See https://developer.github.com/v3/orgs/members/#response-if-requester-is-an-organization-member-and-user-is-not-a-member
-      found = false
+    // User with write permissions
+    if (repo) {
+      found = hasWritePermissions(token, repo, author)
     }
+    if (!found) {
+      found = isElasticMember(token, author)
+    }
+  }
+  return found
+}
+
+def hasWritePermissions(token, repo, author) {
+  return githubPrCheckApproved.hasWritePermission(token, repo, author)
+}
+
+def isElasticMember(token, author) {
+  def found = false
+  try {
+    // Either a user from the org or a user with write permissions
+    def membershipResponse = githubApiCall(token: token, allowEmptyResponse: true,
+                                          url: "https://api.github.com/orgs/elastic/members/${author}")
+    // githubApiCall returns either a raw output or an error message if so it means the user is not a member.
+    found = membershipResponse.message?.trim() ? false : true
+  } catch(err) {
+    log(level: 'WARN', text: "isCommentTrigger: only users under the Elastic organisation are allowed. Message: See ${err.toString()}")
+    // Then it means 404 errorcode.
+    // See https://developer.github.com/v3/orgs/members/#response-if-requester-is-an-organization-member-and-user-is-not-a-member
+    found = false
   }
   return found
 }
