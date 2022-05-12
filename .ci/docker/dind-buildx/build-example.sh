7ca2022722234cbde55ef5d29a00d6bcd760e93d
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2022-05-12 20:05:59 +0200

    fix: review exemple

--- .ci/docker/dind-buildx/build-example.sh
+++ .ci/docker/dind-buildx/build-example.sh
@@ -2,13 +2,19 @@
 #
 # This script show how to use the dind-buildx Docker image to build a Docker image for multiple architectures.
 
+export DOCKER_USER=${DOCKER_USER:?-"Missing environment variable."}
+export DOCKER_PASSWORD=${DOCKER_PASSWORD:?-"Missing environment variable."}
+export DOCKER_REGISTRY=${DOCKER_REGISTRY:-"docker.elastic.co"}
 
-DOCKER_USER=${DOCKER_USER:?-"Missing environment variable."}
-DOCKER_PASSWORD=${DOCKER_PASSWORD:?-"Missing environment variable."}
-DOCKER_REGISTRY=${DOCKER_REGISTRY:-"docker.elastic.co"}
-
-docker run -it -e DOCKER_USER -e DOCKER_PASSWORD -e DOCKER_REGISTRY -v /var/run/docker.sock:/var/run/docker.sock -v "$(pwd):/app -w /app docker.elastic.co/observability-ci/dind-buildx:20.10.14" """
-docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD} ${DOCKER_REGISTRY}
+docker run -it \
+  -e DOCKER_USER \
+  -e DOCKER_PASSWORD \
+  -e DOCKER_REGISTRY \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v "$(pwd):/app" \
+  -w /app \
+  docker.elastic.co/observability-ci/dind-buildx:20.10.14 """
+docker login -u ${DOCKER_USER}
 docker buildx ls
 echo 'Create builder'
 docker buildx create --name mybuilder
