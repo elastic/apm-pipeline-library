512c331aadcbfd7fcaeef342b5d050c53491d9a9
Manuel de la Peña <mdelapenya@gmail.com>
2022-07-21 13:06:49 +0200

    chore: build and push stack images every hour

--- .ci/jobDSL/jobs/apm-ci/apm-shared/docker-images/stack_docker_images.groovy
+++ .ci/jobDSL/jobs/apm-ci/apm-shared/docker-images/stack_docker_images.groovy
@@ -30,7 +30,7 @@ pipelineJob("apm-shared/oblt-test-env/stack-docker-images") {
     artifactDaysToKeep(-1)
   }
   triggers {
-    cron('H H(3-4) * * 1-5')
+    cron('0 */1 * * 1-5')
   }
   definition {
     cpsScm {
