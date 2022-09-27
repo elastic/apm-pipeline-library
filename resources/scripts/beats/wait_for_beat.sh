daa36f4fd36a49a6a8ac84587d9e3e03217f02bc
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-09-27 15:06:17 +0100

    less verbose

--- resources/scripts/beats/wait_for_beat.sh
+++ resources/scripts/beats/wait_for_beat.sh
@@ -6,12 +6,6 @@ URL=${2:-"http://localhost:5066/stats?pretty"}
 echo "INFO: print existing docker context"
 docker ps -a || true
 
-echo "DEBUG: print docker inspect"
-docker inspect "${ID}"
-
-echo "DEBUG: print existing docker logs"
-docker logs -f "${ID}"
-
 echo "INFO: wait for the docker container to be available"
 N=0
 until docker exec "${ID}" curl -sSfI --retry 10 --retry-delay 5 --max-time 5 "${URL}"
