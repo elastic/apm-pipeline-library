1f71f43a2fd1404fcbed4b052667f216558ee499
Victor Martinez <victormartinezrubio@gmail.com>
2023-01-24 11:16:47 +0000

    action: release with GH actions (#2019)

--- resources/scripts/jenkins/release-perform.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -uxeo pipefail
-
-BRANCH_NAME=${BRANCH_NAME:?"missing BRANCH_NAME"}
-
-git checkout -f "${BRANCH_NAME}"
-./mvnw release:prepare release:perform -V --batch-mode -Darguments="-DskipTests=true --batch-mode"
