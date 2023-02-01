a27b79bff7542e4577bd0d8099ce2c1380b176dd
Victor Martinez <VictorMartinezRubio@gmail.com>
2023-02-01 16:30:51 +0100

    add context

--- .ci/scripts/test-golang-version.sh
+++ .ci/scripts/test-golang-version.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
-grep -v -q "$1" vars/goDefaultVersion.groovy
-
-exit $?
+if grep -q "$1" vars/goDefaultVersion.groovy ; then
+  echo "found $1. No need to do nothing"
+  # return false
+  exit 1
+fi
+echo "not found $1. Need to update the file"
+# return true
+exit 0
