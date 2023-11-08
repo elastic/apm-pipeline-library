ec0c0b4823c731cc52f8dfc0b6f573c2a0671511
Adrien Mannocci <adrien.mannocci@elastic.co>
2023-11-08 16:22:35 +0100

    fix: remove useless sourcemap
    Signed-off-by: Adrien Mannocci <adrien.mannocci@elastic.co>

--- .github/actions/branch-lock/dist/index.js
+++ .github/actions/branch-lock/dist/index.js
@@ -1,4 +1,4 @@
-require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
+/******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
 /***/ 7351:
@@ -54584,4 +54584,3 @@ const main_1 = __nccwpck_require__(399);
 module.exports = __webpack_exports__;
 /******/ })()
 ;
-//# sourceMappingURL=index.js.map
