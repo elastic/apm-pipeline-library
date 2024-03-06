5471e616792f735424289da6a5892007a78e65b5
Victor Martinez <VictorMartinezRubio@gmail.com>
2024-03-06 15:04:49 +0100

    oblt-cli: pin version 6.5.3

--- .github/actions/setup-oblt-cli/download.sh
+++ .github/actions/setup-oblt-cli/download.sh
@@ -8,18 +8,18 @@ mkdir -p "${BIN_DIR}"
 
 if [[ ${RUNNER_OS} == "Linux" ]]; then
   if [[ ${RUNNER_ARCH} == "X64" ]]; then
-    gh release download --repo elastic/observability-test-environments -p '*linux_amd64.tar.gz' --output - | tar -xz -C "${BIN_DIR}"
+    PATTERN='*linux_amd64.tar.gz'
   elif [[ ${RUNNER_ARCH} == "ARM64" ]]; then
-    gh release download --repo elastic/observability-test-environments -p '*linux_arm64.tar.gz' --output - | tar -xz -C "${BIN_DIR}"
+    PATTERN='*linux_arm64.tar.gz'
   else
     echo "Unsupported architecture for ${RUNNER_OS}: ${RUNNER_ARCH}"
     exit 1
   fi
 elif [[ ${RUNNER_OS} == "macOS" ]]; then
   if [[ ${RUNNER_ARCH} == "X64" ]]; then
-      gh release download --repo elastic/observability-test-environments -p '*darwin_amd64.tar.gz' --output - | tar -xz -C "${BIN_DIR}"
+      PATTERN='*darwin_amd64.tar.gz'
     elif [[ ${RUNNER_ARCH} == "ARM64" ]]; then
-      gh release download --repo elastic/observability-test-environments -p '*darwin_arm64.tar.gz' --output - | tar -xz -C "${BIN_DIR}"
+      PATTERN='*darwin_arm64.tar.gz'
     else
       echo "Unsupported architecture for ${RUNNER_OS}: ${RUNNER_ARCH}"
       exit 1
@@ -28,5 +28,12 @@ else
   echo "Unsupported OS: ${RUNNER_OS}"
   exit 1
 fi
+
+echo "::warning::It will download the version 6.5.3. Latest version does not support Vault access anymore."
+# TODO: use the latest available version.
+gh release download 6.5.3 \
+  --repo elastic/observability-test-environments \
+  -p "${PATTERN}" \
+  --output - | tar -xz -C "${BIN_DIR}"
 
 echo "${BIN_DIR}" >> "${GITHUB_PATH}"
